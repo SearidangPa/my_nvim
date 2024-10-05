222f0c0e10dc0ef0b3e4c27b742e6a3a6e40832b
Searidang Pa <dangsyncpa@gmail.com>
2024-10-05 16:38:03 -0400

    holy-mother-of-god-it-works

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -87,16 +87,16 @@ return {
   },
 
   {
-    "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  build = ":Copilot auth",
-  opts = {
-    suggestion = { enabled = false },
-    panel = { enabled = false },
-    filetypes = {
-      markdown = true,
-      help = true,
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    build = ':Copilot auth',
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
     },
   },
-  }
 }
