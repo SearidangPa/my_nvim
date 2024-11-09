a999ca0425dd903d511c41d1829d31f884edba6b
SearidangPa <dangsyncpa@gmail.com>
2024-11-09 12:54:57 -0500

    save-harpoon

--- init.lua
+++ init.lua
@@ -14,35 +14,20 @@ vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.inccommand = 'split' -- Preview substitutions live, as you type!
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.shell = 'powershell.exe'
 
 vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
 end)
 
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
-vim.opt.cursorline = true
-vim.opt.scrolloff = 10
-vim.opt.shell = 'powershell.exe'
-
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
--- Diagnostic keymaps
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlights' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
---
+++ lua/kickstart/plugins/harpoon.lua
@@ -0,0 +1,24 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+
+  config = function()
+    local harpoon = require 'harpoon'
+    harpoon:setup {}
+
+    vim.keymap.set('n', '<m-a>', function()
+      harpoon:list():add()
+    end, { desc = 'add to harpoon' })
+
+    vim.keymap.set('n', '<m-l>', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = 'Harpoon (Default)' })
+
+    for _, idx in ipairs { 1, 2, 3, 4, 5 } do
+      vim.keymap.set('n', string.format('<m-%d>', idx), function()
+        harpoon:list():select(idx)
+      end, { desc = string.format('harpoon %d', idx) })
+    end
+  end,
+}
