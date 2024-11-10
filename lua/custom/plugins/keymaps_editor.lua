87498f05df112e5691f7200b796003820b3e344f
SearidangPa <dangsyncpa@gmail.com>
2024-11-10 15:04:13 -0500

    almost

--- lua/custom/plugins/keymaps_editor.lua
+++ lua/custom/plugins/keymaps_editor.lua
@@ -4,6 +4,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- paste
+vim.api.nvim_set_keymap('v', 'p', '"_dP', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'dD', '"_dd', { noremap = true, silent = true })
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
@@ -13,15 +17,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlights' })
 
--- paste
-vim.api.nvim_set_keymap('v', 'p', '"_dP', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'dD', '"_dd', { noremap = true, silent = true })
-
 -- add empty line
 vim.api.nvim_set_keymap('n', '<m-k>', 'O<Esc>j', { noremap = true, silent = true, desc = 'Insert empty line above' })
 vim.api.nvim_set_keymap('n', '<m-j>', 'o<Esc>k', { noremap = true, silent = true, desc = 'Insert empty line below' })
 
 -- empty parentheses
 vim.api.nvim_set_keymap('i', '<m-p>', '()<Esc>a', { noremap = true, silent = true, desc = 'Insert parentheses' })
+
+-- press jj in insert mode to escaped to normal mode
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, silent = true })
 
 return {}
