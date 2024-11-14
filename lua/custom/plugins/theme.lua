5af9dbe51c27dddfa320c1eb19e5508387b185b8
Searidang Pa <dangsyncpa@gmail.com>
2024-11-14 12:44:21 -0500

    save

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,12 +1,22 @@
 return {
-  'rebelot/kanagawa.nvim',
-  priority = 1000,
-  init = function()
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'kanagawa'
+  {
+    'rebelot/kanagawa.nvim',
+    priority = 1000,
+    init = function()
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'kanagawa'
 
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
-  end,
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  { 'rose-pine/neovim', name = 'rose-pine' },
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
 }
