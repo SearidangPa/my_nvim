1825aaaaafcaf055dee388039a3b2f45d424e98e
SearidangPa <dangsyncpa@gmail.com>
2024-11-18 20:53:32 -0500

    save-quick

--- lua/custom/plugins/keymaps_diagnostic.lua
+++ lua/custom/plugins/keymaps_diagnostic.lua
@@ -5,20 +5,19 @@ local function toggle_quickfix()
     vim.cmd 'copen'
   end
 end
-vim.keymap.set('n', ',,', toggle_quickfix, { desc = 'toggle diagnostic windows' })
 
 vim.keymap.set('n', ',q', function()
   vim.diagnostic.setqflist()
   vim.cmd 'copen'
 end, { desc = 'Populate the Quickfix list with diagnostics' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- navigate diagnostics
 vim.keymap.set('n', '<leader>n', ':cnext<CR>', { desc = 'Next Quickfix item' })
 vim.keymap.set('n', '<leader>p', ':cprevious<CR>', { desc = 'Previous Quickfix item' })
 vim.keymap.set('n', ']g', vim.diagnostic.goto_next, { desc = 'Next diagnostic' })
 vim.keymap.set('n', '[g', vim.diagnostic.goto_prev, { desc = 'Previous diagnostic' })
 
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-vim.keymap.set('n', '<m-c>', ':cclose<CR>', { desc = 'Close Quickfix window' })
+vim.keymap.set('n', ',c', ':cclose<CR>', { desc = 'Close Quickfix window' })
+vim.keymap.set('n', ',,', toggle_quickfix, { desc = 'toggle diagnostic windows' })
 
 return {}
