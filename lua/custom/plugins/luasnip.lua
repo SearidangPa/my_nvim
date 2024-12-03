a622141a64865078de242fad10be22b539d62de1
Searidang Pa <dangsyncpa@gmail.com>
2024-12-02 22:27:08 -0500

    save

---
+++ lua/custom/plugins/luasnip.lua
@@ -0,0 +1,77 @@
+return {
+  'L3MON4D3/LuaSnip',
+  build = (function()
+    return 'make install_jsregexp'
+  end)(),
+
+  config = function()
+    local path_sep = package.config:sub(1, 1) -- Detects the path separator (e.g., '\' on Windows, '/' on Unix)
+    local snippet_path = vim.fn.stdpath 'config' .. path_sep .. 'snippets'
+    require('luasnip.loaders.from_lua').load { paths = { snippet_path } }
+
+    local ls = require 'luasnip'
+    vim.snippet.expand = ls.lsp_expand
+
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.snippet.active = function(filter)
+      filter = filter or {}
+      filter.direction = filter.direction or 1
+
+      if filter.direction == 1 then
+        return ls.expand_or_jumpable()
+      else
+        return ls.jumpable(filter.direction)
+      end
+    end
+
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.snippet.jump = function(direction)
+      if direction == 1 then
+        if ls.expandable() then
+          return ls.expand_or_jump()
+        end
+        return ls.jumpable(1) and ls.jump(1)
+      end
+
+      return ls.jumpable(-1) and ls.jump(-1)
+    end
+
+    local types = require 'luasnip.util.types'
+
+    ls.config.set_config {
+      history = true,
+      updateevents = 'TextChanged,TextChangedI',
+      enable_autosnippets = true,
+
+      ext_opts = {
+        [types.choiceNode] = {
+          active = {
+            virt_text = { { '<-', 'Error' } },
+          },
+        },
+      },
+    }
+
+    vim.keymap.set({ 'i', 's' }, '<c-k>', function()
+      return vim.snippet.active { direction = 1 } and vim.snippet.jump(1)
+    end, { silent = true })
+
+    vim.keymap.set({ 'i', 's' }, '<c-j>', function()
+      return vim.snippet.active { direction = -1 } and vim.snippet.jump(-1)
+    end, { silent = true })
+
+    vim.keymap.set('i', '<C-l>', function()
+      if ls.choice_active() then
+        return ls.change_choice(1)
+      end
+    end, { silent = true })
+  end,
+  dependencies = {
+    {
+      'rafamadriz/friendly-snippets',
+      config = function()
+        require('luasnip.loaders.from_vscode').lazy_load()
+      end,
+    },
+  },
+}
