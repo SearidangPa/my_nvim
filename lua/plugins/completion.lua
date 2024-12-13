a64b8a7a4877f73f2b7dfab2a3245dd96b8131c5
Searidang Pa <dangsyncpa@gmail.com>
2024-12-13 17:20:09 -0500

    save

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -30,31 +30,15 @@ return { -- Autocompletion
 
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
-
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
         ['<C-y>'] = cmp.mapping.confirm { select = true },
 
         ['<CR>'] = cmp.mapping.confirm { select = false, behavior = cmp.ConfirmBehavior.Replace },
         ['<Tab>'] = cmp.mapping.select_next_item(),
         ['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete {},
-
-        -- Think of <c-l> as moving to the right of your snippet expansion.
-        --  So if you have a snippet that's like:
-        --  function $name($args)
-        --    $body
-        --  end
-        --
-        -- <c-l> will move you to the right of each of the expansion locations.
-        -- <c-h> is similar, except moving you backwards.
-        -- ['<C-l>'] = cmp.mapping(function()
-        --   if luasnip.expand_or_locally_jumpable() then
-        --     luasnip.expand_or_jump()
-        --   end
-        -- end, { 'i', 's' }),
       },
       sources = {
         {
