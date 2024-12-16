99cdb40ab90b10c2d1c253499c9ba7f680d5b45e
Searidang Pa <dangsyncpa@gmail.com>
2024-12-15 23:51:06 -0500

    test one

--- lua/config/test_func_on_save.lua
+++
@@ -1,5 +0,0 @@
-
--- vim.api.nvim_create_user_command('GoTestFuncOnSave', function()
---   local command = { 'go', 'test', '-json', '-v', '-run', GetEnclosingFunctionName() }
---   attach_to_buffer(vim.api.nvim_get_current_buf(), command)
--- end, {})
