3b63a3b3b5385494267d626666cfdfb41c6a364a
Searidang Pa <dangsyncpa@gmail.com>
2024-12-17 00:47:37 -0500

    save

--- lua/config/nvim_term.lua
+++ lua/config/nvim_term.lua
@@ -13,12 +13,18 @@ vim.api.nvim_create_autocmd('TermOpen', {
   end,
 })
 
+local job_id = 0
 vim.keymap.set('n', '<leader>bt', function()
   vim.cmd.vnew()
   vim.cmd.term()
   vim.cmd.wincmd 'J'
   vim.api.nvim_win_set_height(0, 15)
   vim.api.nvim_feedkeys('i', 'n', true)
+  job_id = vim.bo.channel
 end)
+
+vim.keymap.set('n', '<leader>xst', function()
+  vim.fn.chansend(job_id, 're;st\n')
+end, { desc = 'Send re;st to terminal' })
 
 return {}
