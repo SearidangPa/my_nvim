4456ad0de1dd4875df9e98e59532223275c13678
Searidang Pa <dangsyncpa@gmail.com>
2024-12-16 22:42:59 -0500

    save

--- lua/config/keymaps_shell.lua
+++ lua/config/keymaps_shell.lua
@@ -1,3 +1,14 @@
+local output = {}
+local errors = {}
+
+vim.api.nvim_create_user_command('ViewOutput', function()
+  Create_floating_window(output, 0, -1)
+end, {})
+
+vim.api.nvim_create_user_command('ViewErrors', function()
+  Create_floating_window(errors, 0, -1)
+end, {})
+
 vim.api.nvim_create_user_command('Make', function()
   local cmd
   if vim.fn.has 'win32' == 1 then
@@ -6,25 +17,31 @@ vim.api.nvim_create_user_command('Make', function()
     cmd = { 'make', '-j', 'all' }
   end
 
-  local errors = {}
   local job_id = vim.fn.jobstart(cmd, {
+    stdout_buffered = true,
+    stderr_buffered = true,
+
+    on_stdout = function(_, data)
+      for _, line in ipairs(data) do
+        table.insert(output, line)
+      end
+    end,
+
     on_stderr = function(_, data)
+      for _, line in ipairs(data) do
+        table.insert(errors, line)
+      end
       if data and #data > 0 then
         vim.list_extend(errors, data)
       end
     end,
 
     on_exit = function(_, code)
-      if code == 0 then
-        vim.notify('Make completed successfully!', vim.log.levels.INFO)
+      if code ~= 0 then
+        vim.notify('Make failed with exit code ' .. code, vim.log.levels.ERROR)
         return
       end
-      vim.notify('Make failed with exit code ' .. code, vim.log.levels.ERROR)
-      vim.fn.setqflist({}, ' ', {
-        title = 'Make Errors',
-        lines = errors,
-      })
-      vim.cmd 'copen'
+      vim.notify('Make completed successfully!', vim.log.levels.INFO)
     end,
   })
 
@@ -36,24 +53,28 @@ end, {})
 vim.api.nvim_create_user_command('GoModTidy', function()
   local cmd = { 'go', 'mod', 'tidy' }
 
-  local errors = {}
   local job_id = vim.fn.jobstart(cmd, {
-    on_stderr = function(_, data)
-      if data and #data > 0 then
-        vim.list_extend(errors, data)
+    stdout_buffered = true,
+    stderr_buffered = true,
+
+    on_stdout = function(_, data)
+      for _, line in ipairs(data) do
+        table.insert(output, line)
       end
     end,
-    on_exit = function(_, code)
-      if code == 0 then
-        vim.notify('GoModTidy completed successfully!', vim.log.levels.INFO)
-      else
-        vim.notify('GoModTidy failed with exit code ' .. code, vim.log.levels.ERROR)
-        vim.fn.setqflist({}, ' ', {
-          title = 'GoModTidy Errors',
-          lines = errors,
-        })
-        vim.cmd 'copen'
+
+    on_stderr = function(_, data)
+      for _, line in ipairs(data) do
+        table.insert(errors, line)
       end
+    end,
+
+    on_exit = function(_, code)
+      if code ~= 0 then
+        vim.notify('GoModTidy failed with exit code ' .. code, vim.log.levels.ERROR)
+        return
+      end
+      vim.notify('GoModTidy completed successfully!', vim.log.levels.INFO)
     end,
   })
 
@@ -65,16 +86,14 @@ end, {})
 vim.keymap.set('n', '<leader>gmt', ':GoModTidy<CR>', { desc = '[G]o [M]od [T]idy' })
 vim.api.nvim_set_keymap('n', '<leader>xx', '<cmd>source % <CR>', { noremap = true, silent = true, desc = 'source %' })
 vim.keymap.set('n', '<leader>rm', '<cmd>messages<CR>', { desc = 'read messages' })
+vim.keymap.set('n', '<leader>ma', ':Make<CR>', { desc = 'Run make all in the background' })
+vim.keymap.set('n', '<leader>mc', ':messages clear<CR>', { desc = '[C]lear [m]essages' })
+
+-- shell
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 if vim.fn.has 'win32' == 1 then
   vim.keymap.set('n', '<leader>tt', '<cmd>term powershell.exe<CR>a', { desc = 'Open terminal' })
 else
   vim.keymap.set('n', '<leader>tt', '<cmd>term<CR>a', { desc = 'Open terminal' })
 end
-vim.keymap.set('n', '<leader>ma', ':Make<CR>', { desc = 'Run make all in the background' })
-vim.keymap.set('n', '<leader>mc', ':messages clear<CR>', { desc = '[C]lear [m]essages' })
-vim.keymap.set('n', '<leader>rl', function()
-  vim.cmd 'LspRestart'
-end, { noremap = true, desc = '[R]estart [L]SP' })
-
 return {}
