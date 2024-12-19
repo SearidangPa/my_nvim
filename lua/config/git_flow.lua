b9aea6d9cfec6487df5b9c3d47c9309d6db93050
SearidangPa <dangsyncpa@gmail.com>
2024-12-19 09:45:06 -0500

    ave

--- lua/config/git_flow.lua
+++ lua/config/git_flow.lua
@@ -1,5 +1,6 @@
 local mini_notify = require 'mini.notify'
 local make_notify = mini_notify.make_notify {}
+local commit_msg = ''
 
 local function git_add(on_success)
   vim.fn.jobstart('git add .', {
@@ -14,7 +15,7 @@ local function git_add(on_success)
 end
 
 local function git_commit(on_success)
-  local commit_msg = vim.fn.input 'Commit message: '
+  commit_msg = vim.fn.input 'Commit message: '
   if commit_msg == '' then
     make_notify 'Commit aborted: no message provided.'
     return
@@ -35,7 +36,7 @@ local function git_push()
   vim.fn.jobstart('git push', {
     on_exit = function(_, exit_code)
       if exit_code == 0 then
-        make_notify 'Git push successful!'
+        make_notify(string.format('Git push successful! "%s"', commit_msg))
       else
         make_notify 'Git push failed.'
       end
