3b62614834f3124dbc7ea93ec53ae5b9607c2ac5
SearidangPa <dangsyncpa@gmail.com>
2024-12-19 09:44:06 -0500

    no comment

--- lua/config/git_flow.lua
+++ lua/config/git_flow.lua
@@ -1,7 +1,6 @@
 local mini_notify = require 'mini.notify'
 local make_notify = mini_notify.make_notify {}
 
--- Function to handle 'git add .'
 local function git_add(on_success)
   vim.fn.jobstart('git add .', {
     on_exit = function(_, exit_code)
@@ -14,9 +13,7 @@ local function git_add(on_success)
   })
 end
 
--- Function to handle 'git commit -m "message"'
 local function git_commit(on_success)
-  -- Prompt user for commit message
   local commit_msg = vim.fn.input 'Commit message: '
   if commit_msg == '' then
     make_notify 'Commit aborted: no message provided.'
@@ -34,7 +31,6 @@ local function git_commit(on_success)
   })
 end
 
--- Function to handle 'git push'
 local function git_push()
   vim.fn.jobstart('git push', {
     on_exit = function(_, exit_code)
@@ -47,7 +43,6 @@ local function git_push()
   })
 end
 
--- Main function to orchestrate the workflow
 local function fugitive_git_workflow()
   git_add(function()
     git_commit(function()
@@ -56,7 +51,6 @@ local function fugitive_git_workflow()
   end)
 end
 
--- Create a keymap to trigger the function
 vim.keymap.set('n', '<leader>gcp', fugitive_git_workflow, {
   noremap = true,
   silent = true,
