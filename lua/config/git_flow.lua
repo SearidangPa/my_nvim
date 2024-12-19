fafa2d034a4ecae835a66f6f6ba6abb7e399e2b8
Searidang Pa <dangsyncpa@gmail.com>
2024-12-19 14:55:03 -0500

    Custom input

--- lua/config/git_flow.lua
+++ lua/config/git_flow.lua
@@ -2,13 +2,30 @@ local mini_notify = require 'mini.notify'
 local make_notify = mini_notify.make_notify {}
 local commit_msg = ''
 
-local nui_input = require 'nui.input'
-local event = require('nui.utils.autocmd').event
-
 local width = math.floor(vim.o.columns * 0.9)
 local height = math.floor(vim.o.lines * 0.25)
 local row = math.floor((vim.o.columns - width))
 local col = math.floor((vim.o.lines - height))
+
+local nui_input = require 'nui.input'
+local event = require('nui.utils.autocmd').event
+
+local function perform_commit(on_success_cb)
+  ---@diagnostic disable-next-line: undefined-field
+  local escaped_msg = commit_msg:gsub('"', '\\"') -- Escape double quotes in commit_msg to prevent shell issues
+  local cmd = 'git commit -m "' .. escaped_msg .. '"'
+
+  vim.fn.jobstart(cmd, {
+    on_exit = function(_, exit_code)
+      if exit_code ~= 0 then
+        make_notify 'Git commit failed.'
+        return
+      end
+
+      on_success_cb()
+    end,
+  })
+end
 
 local input = nui_input({
   position = { row = row, col = col },
@@ -18,7 +35,7 @@ local input = nui_input({
   border = {
     style = 'rounded',
     text = {
-      top = '[Howdy?]',
+      top = '[My Lovely Commit Message]',
       top_align = 'center',
     },
   },
@@ -27,19 +44,46 @@ local input = nui_input({
   },
 }, {
   prompt = '> ',
-  default_value = 'Hello',
+  default_value = ' “What is hell? I maintain that it is the suffering of being unable to love.” - Fyodor Dostoevsky',
+
   on_close = function()
     print 'Input Closed!'
   end,
+
   on_submit = function(value)
     print('Input Submitted: ' .. value)
+    commit_msg = value
   end,
 })
 
-input:mount() -- mount/open the component
 input:on(event.BufLeave, function()
   input:unmount() -- unmount component when cursor leaves buffer
 end)
+
+local function handle_choice(choice, on_success_cb)
+  if not choice then
+    make_notify 'Commit aborted: no message selected.'
+    return
+  end
+
+  if choice ~= 'Custom input' then
+    perform_commit(on_success_cb)
+    return
+  end
+
+  input:mount() -- mount/open the component
+
+  perform_commit(on_success_cb)
+
+  -- vim.ui.input({ prompt = 'Enter Commit Message: ' }, function(input_msg)
+  --   if not input_msg or vim.trim(input_msg) == '' then
+  --     make_notify 'Commit aborted: no message provided.'
+  --     return
+  --   end
+  --   commit_msg = input_msg
+  --   perform_commit(on_success_cb)
+  -- end)
+end
 
 local function git_add(on_success_cb)
   vim.fn.jobstart('git add .', {
@@ -70,43 +114,6 @@ local function git_push()
       make_notify(string.format(git_push_format_notification, commit_msg))
     end,
   })
-end
-
-local function perform_commit(on_success_cb)
-  local escaped_msg = commit_msg:gsub('"', '\\"') -- Escape double quotes in commit_msg to prevent shell issues
-  local cmd = 'git commit -m "' .. escaped_msg .. '"'
-
-  vim.fn.jobstart(cmd, {
-    on_exit = function(_, exit_code)
-      if exit_code ~= 0 then
-        make_notify 'Git commit failed.'
-        return
-      end
-
-      on_success_cb()
-    end,
-  })
-end
-
-local function handle_choice(choice, on_success_cb)
-  if not choice then
-    make_notify 'Commit aborted: no message selected.'
-    return
-  end
-
-  if choice ~= 'Custom input' then
-    perform_commit(on_success_cb)
-    return
-  end
-
-  vim.ui.input({ prompt = 'Enter Commit Message: ' }, function(input_msg)
-    if not input_msg or vim.trim(input_msg) == '' then
-      make_notify 'Commit aborted: no message provided.'
-      return
-    end
-    commit_msg = input_msg
-    perform_commit(on_success_cb)
-  end)
 end
 
 local function git_commit_with_message_prompt(on_success_cb)
