b56789dc7d6144e7d9b4dcba024f2e562a7b4f27
Searidang Pa <dangsyncpa@gmail.com>
2024-12-21 11:56:03 -0500

    sinp

--- lua/config/go_snip.lua
+++ lua/config/go_snip.lua
@@ -313,3 +313,19 @@ ls.add_snippets('go', {
     )
   ),
 })
+
+ls.add_snippets('go', {
+  s(
+    'cli',
+    fmta(
+      [[
+            //go:build windows
+
+            package <finish>
+        ]],
+      {
+        finish = i(0),
+      }
+    )
+  ),
+})
