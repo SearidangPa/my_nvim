1847dd760f1bd2030628a9acfa4b862353db01a5
Searidang Pa <dangsyncpa@gmail.com>
2024-12-25 10:53:45 -0500

    Checkpoint:

--- lua/plugins/teej_present.lua
+++ lua/plugins/teej_present.lua
@@ -1,8 +1,4 @@
 return {
   'tjdevries/present.nvim',
-  config = function()
-    vim.api.nvim_create_user_command('PresentMarkdown', function()
-      require('present').start_presentation {}
-    end, {})
-  end,
+  config = function() end,
 }
