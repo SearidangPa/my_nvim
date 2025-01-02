368124e340dbec25510693c31a13bacc09c95432
Searidang Pa <dangsyncpa@gmail.com>
2025-01-02 14:07:26 +0700

    Half-progress:

--- lua/config/multigrep.lua
+++ lua/config/multigrep.lua
@@ -6,28 +6,52 @@ local themes = require 'telescope.themes'
 
 local M = {}
 
-local live_multigrep = function(opts)
+local construct_args_multigrep = function(prompt)
+  if not prompt or prompt == '' then
+    return nil
+  end
+  local pieces = vim.split(prompt, '  ')
+  local args = { 'rg' }
+
+  if pieces[1] then
+    table.insert(args, '-e')
+    table.insert(args, pieces[1])
+  end
+
+  if pieces[2] then
+    table.insert(args, '-g')
+    table.insert(args, pieces[2])
+  end
+  return args
+end
+
+local construct_args_no_regex = function(prompt)
+  if not prompt or prompt == '' then
+    return nil
+  end
+  local pieces = vim.split(prompt, '  ')
+  local args = { 'rg' }
+
+  if pieces[1] then
+    table.insert(args, '-F')
+    table.insert(args, pieces[1])
+  end
+  return args
+end
+
+local live_search = function(opts)
   opts = opts or {}
   opts.cwd = opts.cwd or vim.uv.cwd()
   opts = vim.tbl_deep_extend('force', themes.get_ivy(), opts)
+  assert(opts.args_constructor, 'You need to pass an args_constructor')
+  assert(opts.prompt_title, 'You need to pass a prompt_title')
+
   local finder = finders.new_async_job {
     command_generator = function(prompt)
-      if not prompt or prompt == '' then
+      local args = opts.args_constructor(prompt)
+      if not args then
         return nil
       end
-      local pieces = vim.split(prompt, '  ')
-      local args = { 'rg' }
-
-      if pieces[1] then
-        table.insert(args, '-e')
-        table.insert(args, pieces[1])
-      end
-
-      if pieces[2] then
-        table.insert(args, '-g')
-        table.insert(args, pieces[2])
-      end
-
       ---@diagnostic disable-next-line: deprecated
       return vim.tbl_flatten {
         args,
@@ -48,7 +72,7 @@ local live_multigrep = function(opts)
   pickers
     .new(opts, {
       debounce = 100,
-      prompt_title = 'Multi grep',
+      prompt_title = opts.prompt_title,
       finder = finder,
       previewer = conf.grep_previewer(opts),
       sorter = require('telescope.sorters').empty(),
@@ -56,53 +80,20 @@ local live_multigrep = function(opts)
     :find()
 end
 
-local live_no_regex = function(opts)
-  opts = opts or {}
-  opts.cwd = opts.cwd or vim.uv.cwd()
-  opts = vim.tbl_deep_extend('force', themes.get_ivy(), opts)
-  local finder = finders.new_async_job {
-    command_generator = function(prompt)
-      if not prompt or prompt == '' then
-        return nil
-      end
-      local pieces = vim.split(prompt, '  ')
-      local args = { 'rg' }
-
-      if pieces[1] then
-        table.insert(args, '-F')
-        table.insert(args, pieces[1])
-      end
-
-      ---@diagnostic disable-next-line: deprecated
-      return vim.tbl_flatten {
-        args,
-        {
-          '--color=never',
-          '--no-heading',
-          '--with-filename',
-          '--line-number',
-          '--column',
-          '--smart-case',
-        },
-      }
-    end,
-    entry_maker = make_entry.gen_from_vimgrep(opts),
-    cwd = opts.cwd,
-  }
-
-  pickers
-    .new(opts, {
-      debounce = 100,
-      prompt_title = 'No regex',
-      finder = finder,
-      previewer = conf.grep_previewer(opts),
-      sorter = require('telescope.sorters').empty(),
-    })
-    :find()
-end
 M.setup = function()
-  vim.keymap.set('n', '<leader>sm', live_multigrep, { desc = '[S]earch [M]ulti grep' })
-  vim.keymap.set('n', '<leader>sn', live_no_regex, { desc = '[S]earch [N]o regex' })
+  vim.keymap.set('n', '<leader>sm', function()
+    live_search {
+      args_constructor = construct_args_multigrep,
+      prompt_title = 'multi grep',
+    }
+  end, { desc = '[S]earch [M]ulti grep' })
+
+  vim.keymap.set('n', '<leader>sn', function()
+    live_search {
+      args_constructor = construct_args_no_regex,
+      prompt_title = 'no regex',
+    }
+  end, { desc = '[S]earch [N]o regex' })
 end
 
 return M
