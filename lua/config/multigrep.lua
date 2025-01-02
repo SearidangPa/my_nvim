80528eb6ba70134821ce3454d42cff5fa299ff1a
Searidang Pa <dangsyncpa@gmail.com>
2025-01-02 13:53:05 +0700

    Checkpoint:

--- lua/config/multigrep.lua
+++ lua/config/multigrep.lua
@@ -56,8 +56,53 @@ local live_multigrep = function(opts)
     :find()
 end
 
+local live_no_regex = function(opts)
+  opts = opts or {}
+  opts.cwd = opts.cwd or vim.uv.cwd()
+  opts = vim.tbl_deep_extend('force', themes.get_ivy(), opts)
+  local finder = finders.new_async_job {
+    command_generator = function(prompt)
+      if not prompt or prompt == '' then
+        return nil
+      end
+      local pieces = vim.split(prompt, '  ')
+      local args = { 'rg' }
+
+      if pieces[1] then
+        table.insert(args, '-F')
+        table.insert(args, pieces[1])
+      end
+
+      ---@diagnostic disable-next-line: deprecated
+      return vim.tbl_flatten {
+        args,
+        {
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case',
+        },
+      }
+    end,
+    entry_maker = make_entry.gen_from_vimgrep(opts),
+    cwd = opts.cwd,
+  }
+
+  pickers
+    .new(opts, {
+      debounce = 100,
+      prompt_title = 'No regex',
+      finder = finder,
+      previewer = conf.grep_previewer(opts),
+      sorter = require('telescope.sorters').empty(),
+    })
+    :find()
+end
 M.setup = function()
   vim.keymap.set('n', '<leader>sm', live_multigrep, { desc = '[S]earch [M]ulti grep' })
+  vim.keymap.set('n', '<leader>sn', live_no_regex, { desc = '[S]earch [N]o regex' })
 end
 
 return M
