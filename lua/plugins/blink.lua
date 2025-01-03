08648e9daf5c4f31c8b26ded6131807e6393853c
Searidang Pa <dangsyncpa@gmail.com>
2025-01-03 21:31:47 +0700

    Half-progress:

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,3 +1,19 @@
+local function generate_keymap()
+  local keymap = {}
+  keymap.preset = 'default' -- Explicit assignment to avoid conflicts
+
+  for i = 1, 10 do
+    local key = i == 10 and '<A-0>' or ('<A-' .. i .. '>')
+    keymap[key] = {
+      function(cmp)
+        cmp.accept { index = i }
+      end,
+    }
+  end
+
+  return keymap
+end
+
 return {
   {
     'saghen/blink.cmp',
@@ -32,59 +48,23 @@ return {
         end,
       },
 
-      keymap = {
-        preset = 'default',
-        ['<A-1>'] = {
-          function(cmp)
-            cmp.accept { index = 1 }
-          end,
-        },
-        ['<A-2>'] = {
-          function(cmp)
-            cmp.accept { index = 2 }
-          end,
-        },
-        ['<A-3>'] = {
-          function(cmp)
-            cmp.accept { index = 3 }
-          end,
-        },
-        ['<A-4>'] = {
-          function(cmp)
-            cmp.accept { index = 4 }
-          end,
-        },
-        ['<A-5>'] = {
-          function(cmp)
-            cmp.accept { index = 5 }
-          end,
-        },
-        ['<A-6>'] = {
-          function(cmp)
-            cmp.accept { index = 6 }
-          end,
-        },
-        ['<A-7>'] = {
-          function(cmp)
-            cmp.accept { index = 7 }
-          end,
-        },
-        ['<A-8>'] = {
-          function(cmp)
-            cmp.accept { index = 8 }
-          end,
-        },
-        ['<A-9>'] = {
-          function(cmp)
-            cmp.accept { index = 9 }
-          end,
-        },
-        ['<A-0>'] = {
-          function(cmp)
-            cmp.accept { index = 10 }
-          end,
+      completion = {
+        menu = {
+          draw = {
+            columns = { { 'item_idx' }, { 'kind_icon' }, { 'label', 'label_description', gap = 1 } },
+            components = {
+              item_idx = {
+                text = function(ctx)
+                  return ctx.idx == 10 and '0' or ctx.idx >= 10 and ' ' or tostring(ctx.idx)
+                end,
+                highlight = 'BlinkCmpItemIdx', -- optional, only if you want to change its color
+              },
+            },
+          },
         },
       },
+
+      keymap = generate_keymap(),
     },
   },
 }
