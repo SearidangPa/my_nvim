6918bbf9c1d049863104cc4d11cd7325f046450e
Searidang Pa <dangsyncpa@gmail.com>
2025-01-03 21:44:06 +0700

    Half-progress:

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -61,6 +61,10 @@ return {
             },
           },
         },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500,
+        },
       },
 
       keymap = generate_keymap(),
