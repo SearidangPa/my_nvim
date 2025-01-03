b75694d0e93506d1570d2e8a53d80b5ec948ac4f
Searidang Pa <dangsyncpa@gmail.com>
2025-01-03 21:17:17 +0700

    Checkpoint: copilot

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,18 +1,3 @@
-local function SuggestLines(n)
-  vim.fn['copilot#Accept'] ''
-  local queuedText = vim.fn['copilot#TextQueuedForInsertion']() or ''
-  queuedText = queuedText:match '^%s*(.-)%s*$' or ''
-  local lines = {}
-  for line in queuedText:gmatch '[^\n]+' do
-    table.insert(lines, line)
-  end
-  local selectedLines = vim.list_slice(lines, 1, n or #lines)
-  for i, line in ipairs(selectedLines) do
-    selectedLines[i] = line:match '^%s*(.-)$' -- Trim leading spaces/tabs
-  end
-  return table.concat(selectedLines, '\n') .. '\n'
-end
-
 local function toggle_quickfix()
   if vim.fn.getwininfo(vim.fn.win_getid())[1].quickfix == 1 then
     vim.cmd 'cclose'
@@ -79,14 +64,18 @@ end, { desc = 'Open diagnostic float' })
 
 map('i', '<M-f>', function()
   return vim.fn['copilot#AcceptWord'] ''
-end, { expr = true, remap = false })
+end, { expr = true, remap = false, desc = 'Copilot Accept word' })
 
-for i = 1, 9 do
-  local key = string.format('<M-%d>', i)
-  map('i', key, function()
-    return SuggestLines(i)
-  end, { expr = true, remap = false })
-end
+map('i', '<M-l>', function()
+  return vim.fn['copilot#AcceptLine'] ''
+end, { expr = true, remap = false, desc = 'Copilot Accept line' })
+
+map('i', '<M-y>', function()
+  local accept = vim.fn['copilot#Accept']
+  local res = accept(vim.api.nvim_replace_termcodes('<Tab>', true, true, false))
+  res = res .. '\n'
+  vim.api.nvim_feedkeys(res, 'n', false)
+end, { expr = true, remap = false, desc = 'Copilot Accept' })
 
 -- [[
 -- ================== Rename the first letter
