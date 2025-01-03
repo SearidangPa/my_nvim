c5bc9011dcbe0b80be84277da46414ebdfd569a3
Searidang Pa <dangsyncpa@gmail.com>
2025-01-03 15:30:11 +0700

    Half-progress:

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,8 +1,11 @@
 return {
   {
     'saghen/blink.cmp',
-    dependencies = 'rafamadriz/friendly-snippets',
-    version = 'v0.*',
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+      { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+    },
+    version = '*',
     opts = {
       keymap = { preset = 'default' },
       appearance = {
@@ -10,6 +13,25 @@ return {
         nerd_font_variant = 'mono',
       },
       signature = { enabled = true },
+
+      sources = {
+        default = { 'lsp', 'path', 'luasnip', 'buffer' },
+      },
+
+      snippets = {
+        expand = function(snippet)
+          require('luasnip').lsp_expand(snippet)
+        end,
+        active = function(filter)
+          if filter and filter.direction then
+            return require('luasnip').jumpable(filter.direction)
+          end
+          return require('luasnip').in_snippet()
+        end,
+        jump = function(direction)
+          require('luasnip').jump(direction)
+        end,
+      },
     },
   },
 }
