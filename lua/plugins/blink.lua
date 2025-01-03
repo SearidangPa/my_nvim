59e64559e427e794a47cef0fcaaeab23359d53c0
Searidang Pa <dangsyncpa@gmail.com>
2025-01-03 21:18:23 +0700

    Half-progress: my god

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -7,7 +7,6 @@ return {
     },
     version = '*',
     opts = {
-      keymap = { preset = 'default' },
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono',
@@ -31,6 +30,60 @@ return {
         jump = function(direction)
           require('luasnip').jump(direction)
         end,
+      },
+
+      keymap = {
+        preset = 'default',
+        ['<A-1>'] = {
+          function(cmp)
+            cmp.accept { index = 1 }
+          end,
+        },
+        ['<A-2>'] = {
+          function(cmp)
+            cmp.accept { index = 2 }
+          end,
+        },
+        ['<A-3>'] = {
+          function(cmp)
+            cmp.accept { index = 3 }
+          end,
+        },
+        ['<A-4>'] = {
+          function(cmp)
+            cmp.accept { index = 4 }
+          end,
+        },
+        ['<A-5>'] = {
+          function(cmp)
+            cmp.accept { index = 5 }
+          end,
+        },
+        ['<A-6>'] = {
+          function(cmp)
+            cmp.accept { index = 6 }
+          end,
+        },
+        ['<A-7>'] = {
+          function(cmp)
+            cmp.accept { index = 7 }
+          end,
+        },
+        ['<A-8>'] = {
+          function(cmp)
+            cmp.accept { index = 8 }
+          end,
+        },
+        ['<A-9>'] = {
+          function(cmp)
+            cmp.accept { index = 9 }
+          end,
+        },
+        ['<A-0>'] = {
+          function(cmp)
+            cmp.accept { index = 10 }
+          end,
+        },
       },
     },
   },
