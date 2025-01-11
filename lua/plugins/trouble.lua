cdc285346d92a2dda166a5122453dfc4a64279a2
Searidang Pa <dangsyncpa@gmail.com>
2025-01-11 21:25:59 +0700

    Save progress:

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -13,24 +13,24 @@ return {
         desc = 'Toggle Trouble Diagnostics',
       },
       {
-        '<localleader>xd',
+        '<localleader>td',
         '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
         desc = 'Toggle [D]iagnostics for the current buffer',
       },
       {
-        '<leader>xs',
+        '<leader>ts',
         '<cmd>Trouble symbols toggle focus=false<cr>',
         desc = '[T]oggle [S]ymbols',
       },
       {
-        '<leader>xl',
+        '<leader>tl',
         '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
         desc = '[T]oggle [L]SP Definitions / references / ... (Trouble)',
       },
       {
         '<leader>xq',
         '<cmd>Trouble qflist toggle<cr>',
-        desc = '[T]oggle [Q]uickfix',
+        desc = 'Toggle Trouble Quickfix',
       },
     },
   },
