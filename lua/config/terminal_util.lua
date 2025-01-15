f6f08c62ed0dcf19a2040785d05a7011c6c95eb4
Searidang Pa <dangsyncpa@gmail.com>
2025-01-15 18:02:07 +0700

    Save progress:

--- lua/config/terminal_util.lua
+++ lua/config/terminal_util.lua
@@ -47,7 +47,7 @@ function Send_command_toggle_term(opts)
   end)
 end
 
-function Focus_above_small_terminal(small_term_state)
+local function focus_above_small_terminal(small_term_state)
   if not vim.api.nvim_win_is_valid(small_term_state.win) then
     return
   end
@@ -67,5 +67,34 @@ function Focus_above_small_terminal(small_term_state)
 
   if target_win then
     vim.api.nvim_set_current_win(target_win)
+  end
+end
+
+function Handle_choice(opts)
+  local is_float = opts.is_float
+  local choice = opts.choice
+  local term_state = opts.term_state
+  local toggle_term_func = opts.toggle_term_func
+
+  local channel_id, buf
+  local win = term_state.win
+  if not vim.api.nvim_win_is_valid(win) then
+    win = toggle_term_func()
+  end
+
+  channel_id = term_state.chan
+  buf = term_state.buf
+
+  if choice == '<Ctrl-C>' then
+    vim.fn.chansend(channel_id, '\x03')
+  else
+    vim.fn.chansend(channel_id, string.format('%s\r\n', choice))
+  end
+
+  local line_count = vim.api.nvim_buf_line_count(buf)
+  vim.api.nvim_win_set_cursor(win, { line_count, 0 })
+
+  if not is_float then
+    focus_above_small_terminal(term_state)
   end
 end
