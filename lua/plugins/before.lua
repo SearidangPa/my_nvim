8290d0437d3c6075ba523027dc13551eb7636604
Searidang Pa <dangsyncpa@gmail.com>
2025-01-22 13:43:57 -0500

    Save progress:

--- lua/plugins/before.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'bloznelis/before.nvim',
-  config = function()
-    local before = require 'before'
-    before.setup()
-    vim.keymap.set('n', '<leader>qe', before.show_edits_in_quickfix, { desc = '[Q]uickfix [E]dit history' })
-    vim.keymap.set('n', '<leader>se', before.show_edits_in_telescope, { desc = '[S]earch [E]dit history' })
-  end,
-}
