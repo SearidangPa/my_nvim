929067acbeb678a3fde1a7032860fa57822cfa36
Searidang Pa <dangsyncpa@gmail.com>
2025-01-28 12:21:01 -0500

    Save progress:

--- lua/config/fold_err_blocks.lua
+++ lua/config/fold_err_blocks.lua
@@ -1,4 +1,36 @@
-local function fold_err_blocks(bufnr)
+local function fold_err_if_node(query, root, bufnr)
+  for _, node in query:iter_captures(root, bufnr, 0, -1) do
+    if not node then
+      return
+    end
+
+    local left_text = vim.treesitter.get_node_text(node, bufnr)
+
+    if not string.find(left_text, 'err') then
+      goto continue
+    end
+
+    local current_node = node:parent()
+    while current_node and current_node:type() ~= 'if_statement' do
+      current_node = current_node:parent()
+    end
+
+    if current_node and current_node:type() == 'if_statement' then
+      local start_row, _, end_row, _ = current_node:range()
+      start_row = start_row + 1
+      end_row = end_row
+
+      if start_row <= end_row then
+        vim.cmd(string.format('%d,%dfold', start_row, end_row + 1))
+      end
+    end
+
+    ::continue::
+  end
+end
+
+local function fold_err()
+  local bufnr = vim.api.nvim_get_current_buf()
   local query = vim.treesitter.query.parse(
     'go',
     [[
@@ -17,31 +49,7 @@ local function fold_err_blocks(bufnr)
   local root = tree:root()
   assert(root, 'Tree root is nil')
 
-  for _, node in query:iter_captures(root, bufnr, 0, -1) do
-    if not node then
-      return
-    end
-
-    local left_text = vim.treesitter.get_node_text(node, bufnr)
-    if string.find(left_text, 'err') then
-      local current_node = node:parent()
-      while current_node and current_node:type() ~= 'if_statement' do
-        current_node = current_node:parent()
-      end
-
-      if current_node and current_node:type() == 'if_statement' then
-        local start_row, _, end_row, _ = current_node:range()
-        start_row = start_row + 1
-        end_row = end_row
-
-        if start_row <= end_row then
-          vim.cmd(string.format('%d,%dfold', start_row, end_row + 1))
-        end
-      end
-    end
-  end
+  fold_err_if_node(query, root, bufnr)
 end
 
-vim.api.nvim_create_user_command('FoldErr', function()
-  fold_err_blocks(vim.api.nvim_get_current_buf())
-end, {})
+vim.api.nvim_create_user_command('FoldErr', fold_err, {})
