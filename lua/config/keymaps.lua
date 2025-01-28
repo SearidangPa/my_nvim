440be8c7ce68cfc595b22c24223564202c68e1ee
Searidang Pa <dangsyncpa@gmail.com>
2025-01-27 19:32:30 -0500

    Save progress:

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -141,4 +141,49 @@ vim.keymap.set('n', '<leader>ce', function()
   vim.api.nvim_buf_clear_namespace(0, -1, 0, -1)
 end, { silent = true, desc = '[C]lear [E]xtmarks' })
 
+local function fold_err_blocks(bufnr)
+  local query = vim.treesitter.query.parse(
+    'go',
+    [[
+    (if_statement
+      condition: (binary_expression
+        left: (identifier) @left
+        right: (nil) @right))
+  ]]
+  )
+  local parser = vim.treesitter.get_parser(bufnr, 'go', {})
+  local tree = parser:parse()[1]
+  local root = tree:root()
+  assert(root, 'Tree root is nil')
+
+  for _, node in query:iter_captures(root, bufnr, 0, -1) do
+    if not node then
+      return
+    end
+
+    local left_text = vim.treesitter.get_node_text(node, bufnr)
+    if left_text == 'err' then
+      local current_node = node:parent()
+      while current_node and current_node:type() ~= 'if_statement' do
+        current_node = current_node:parent()
+      end
+
+      if current_node and current_node:type() == 'if_statement' then
+        local start_row, _, end_row, _ = current_node:range()
+        start_row = start_row + 1
+        end_row = end_row
+
+        if start_row <= end_row then
+          vim.api.nvim_win_set_cursor(0, { start_row, 0 })
+          vim.cmd 'normal! za'
+        end
+      end
+    end
+  end
+end
+
+vim.api.nvim_create_user_command('FoldErrBlocks', function()
+  fold_err_blocks(vim.api.nvim_get_current_buf())
+end, {})
+
 return {}
