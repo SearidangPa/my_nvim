5f59edef1e4568f9d1c8369112e12433d6f12891
Searidang Pa <dangsyncpa@gmail.com>
2025-01-28 14:00:42 -0500

    Save progress:

--- lua/plugins/theme_plugins.lua
+++ lua/plugins/theme_plugins.lua
@@ -3,10 +3,6 @@ return {
     'rebelot/kanagawa.nvim',
   },
   { 'rose-pine/neovim', name = 'rose-pine' },
-  { 'folke/tokyonight.nvim', lazy = false },
-  {
-    'navarasu/onedark.nvim',
-  },
   {
     'catppuccin/nvim',
     name = 'catppuccin',
