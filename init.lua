3c942502f061663f06dcf2035154c6e20fb065b1
SearidangPa <dangsyncpa@gmail.com>
2025-01-30 08:05:40 -0500

    Save progress:

--- init.lua
+++ init.lua
@@ -76,4 +76,12 @@ vim.schedule(function()
       require('config.' .. module)
     end
   end
+
+  vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+      vim.highlight.on_yank()
+    end,
+  })
 end)
