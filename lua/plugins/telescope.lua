0978cb1ed61d5615b9c12542303e56d8d47439e1
SearidangPa <dangsyncpa@gmail.com>
2025-02-04 15:46:31 -0500

    Save progress:

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -26,9 +26,9 @@ return {
       map('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
 
       -- have not achieved muscle memory for these yet
+      map('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       map('n', '<leader>st', builtin.git_status, { desc = 'Git [St]atus' })
       map('n', '<leader>sj', builtin.jumplist, { desc = '[S]earch [J]umplist' })
-      map('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       map('n', '<leader>sb', builtin.git_branches, { desc = '[S]earch Git [B]ranches' })
 
       -- useless?
