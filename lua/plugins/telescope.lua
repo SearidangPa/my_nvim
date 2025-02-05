5df13cd09181169abf8f80b4c9efc20838c94cd4
Searidang Pa <dangsyncpa@gmail.com>
2025-02-04 20:15:14 -0500

    Save progress:

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -27,7 +27,7 @@ return {
 
       -- have not achieved muscle memory for these yet
       map('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      map('n', '<leader>st', builtin.git_status, { desc = 'Git [St]atus' })
+      map('n', '<leader>se', builtin.git_status, { desc = '[S]earch [E]dit (unstaged files)' })
       map('n', '<leader>sj', builtin.jumplist, { desc = '[S]earch [J]umplist' })
       map('n', '<leader>sb', builtin.git_branches, { desc = '[S]earch Git [B]ranches' })
 
