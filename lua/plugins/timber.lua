5533544211b367f6e02a65a087c58b8a0a657c2b
Searidang Pa <dangsyncpa@gmail.com>
2025-02-07 21:08:07 -0500

    Save progress:

--- lua/plugins/timber.lua
+++ lua/plugins/timber.lua
@@ -3,6 +3,13 @@ return {
   version = '*', -- Use for stability; omit to use `main` branch for the latest features
   event = 'VeryLazy',
   config = function()
-    require('timber').setup {}
+    local opts = {
+      log_templates = {
+        default = {
+          go = [[log.Printf("=====================> %log_target: %v\n%", log_target)]],
+        },
+      },
+    }
+    require('timber').setup(opts)
   end,
 }
