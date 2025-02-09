b0c006b85b4aaca6fac1b6a4882c7e43326e187b
Searidang Pa <dangsyncpa@gmail.com>
2025-02-08 21:34:19 -0500

    Save progress: prev_method_start

--- lua/config/nav_go_func_decl.lua
+++ lua/config/nav_go_func_decl.lua
@@ -100,9 +100,43 @@ local function move_to_next_func_decl(opts)
   end
 end
 
----@param opts move_opts
-local function move_to_prev_func_decl(opts)
-  local is_end = opts.is_end
+local function prev_func_decl_start(node, cursor_row, cursor_col)
+  local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+  local query = vim.treesitter.query.parse(
+    lang,
+    [[
+      (function_declaration) @func_decl
+    ]]
+  )
+  local bufnr = vim.api.nvim_get_current_buf()
+  local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+  local parser = vim.treesitter.get_parser(bufnr, lang, {})
+  local tree = parser:parse()[1]
+  local root = tree:root()
+  assert(root, 'Tree root is nil')
+
+  local previous_node = nil
+  for _, node in query:iter_captures(root, bufnr, 0, -1) do
+    if node then
+      if not previous_node then
+        previous_node = node
+      end
+
+      local s_row, _, _ = node:start()
+      if s_row > cursor_row then
+        break
+      end
+
+      local prev_s_row, _, _, _ = previous_node:start()
+      if s_row > prev_s_row then
+        previous_node = node
+      end
+    end
+  end
+  return previous_node
+end
+
+local function move_to_prev_func_decl_start()
   local count = vim.v.count
   if count == 0 then
     count = 1
@@ -111,15 +145,10 @@ local function move_to_prev_func_decl(opts)
   for _ = 1, count do
     local cursor_pos = vim.api.nvim_win_get_cursor(0)
     local current_row, current_col = cursor_pos[1] - 1, cursor_pos[2]
-    local previous_node = find_prev_func_decl(root, current_row, current_col)
-
+    local previous_node = prev_func_decl_start(root, current_row, current_col)
     if previous_node then
-      local start_row, start_col, end_row, end_col = previous_node:range { include_bytes = false }
-      if not is_end then
-        vim.api.nvim_win_set_cursor(0, { start_row + 1, start_col })
-      else
-        vim.api.nvim_win_set_cursor(0, { end_row + 1, end_col })
-      end
+      local start_row, start_col, end_row, end_col = previous_node:range()
+      vim.api.nvim_win_set_cursor(0, { start_row + 1, start_col })
     end
   end
 end
@@ -127,9 +156,7 @@ end
 vim.keymap.set('n', ']m', function()
   move_to_next_func_decl { is_end = false }
 end, { desc = 'Next Func Declaraion start' })
-vim.keymap.set('n', '[m', function()
-  move_to_prev_func_decl { is_end = false }
-end, { desc = 'Prev Func Declaraion start' })
+vim.keymap.set('n', '[m', move_to_prev_func_decl_start, { desc = 'Prev Func Declaraion start' })
 
 vim.keymap.set('n', ']M', function()
   move_to_next_func_decl { is_end = true }
