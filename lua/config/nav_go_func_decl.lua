75b6e76652f5e3451388aa9cfdd869623cd80b3a
Searidang Pa <dangsyncpa@gmail.com>
2025-02-08 21:46:18 -0500

    Save progress:

--- lua/config/nav_go_func_decl.lua
+++ lua/config/nav_go_func_decl.lua
@@ -98,6 +98,28 @@ local function prev_func_decl_start(root, query, cursor_row)
   return previous_node
 end
 
+local function prev_func_decl_end(root, query, cursor_row)
+  local previous_node = nil
+  for _, node in query:iter_captures(root, 0, 0, -1) do
+    if node then
+      if not previous_node then
+        previous_node = node
+      end
+
+      local _, _, e_row, _ = node:range()
+      if e_row >= cursor_row then
+        break
+      end
+
+      local _, _, prev_s_row, _ = previous_node:range()
+      if e_row > prev_s_row then
+        previous_node = node
+      end
+    end
+  end
+  return previous_node
+end
+
 local function move_to_prev_func_decl_start()
   local count = vim.v.count
   if count == 0 then
@@ -111,6 +133,25 @@ local function move_to_prev_func_decl_start()
     if previous_node then
       local start_row, start_col, _, _ = previous_node:range()
       vim.api.nvim_win_set_cursor(0, { start_row + 1, start_col })
+      current_row = start_row
+    end
+  end
+end
+
+local function move_to_prev_func_decl_end()
+  local count = vim.v.count
+  if count == 0 then
+    count = 1
+  end
+  local root, query = get_root_node()
+  for _ = 1, count do
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    local current_row = cursor_pos[1] - 1
+    local previous_node = prev_func_decl_end(root, query, current_row)
+    if previous_node then
+      local _, _, end_row, end_col = previous_node:range()
+      vim.api.nvim_win_set_cursor(0, { end_row + 1, end_col })
+      current_row = end_row
     end
   end
 end
@@ -123,3 +164,5 @@ vim.keymap.set('n', '[m', move_to_prev_func_decl_start, { desc = 'Prev Func Decl
 vim.keymap.set('n', ']M', function()
   move_to_next_func_decl { is_end = true }
 end, { desc = 'Next Func Declaraion End' })
+
+vim.keymap.set('n', '[M', move_to_prev_func_decl_end, { desc = 'Prev Func Declaraion End' })
