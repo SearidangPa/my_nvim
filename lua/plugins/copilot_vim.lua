56700e99dc36476d9f700b12252f5a95f7b82304
Searidang Pa <dangsyncpa@gmail.com>
2025-02-18 13:58:37 -0500

    Save progress: check

--- lua/plugins/copilot_vim.lua
+++ lua/plugins/copilot_vim.lua
@@ -1,49 +1,4 @@
 local map = vim.keymap.set
-local function accept()
-  local accept = vim.fn['copilot#Accept']
-  assert(accept, 'copilot#Accept not found')
-  local res = accept()
-  vim.api.nvim_feedkeys(res, 'n', false)
-end
-
-local function accept_with_indent()
-  local accept = vim.fn['copilot#Accept']
-  assert(accept, 'copilot#Accept not found')
-  local res = accept()
-  res = res .. '\r'
-  vim.api.nvim_feedkeys(res, 'n', false)
-end
-
-local function accept_word()
-  local accept_word = vim.fn['copilot#AcceptWord']
-  assert(accept_word, 'copilot#AcceptWord not found')
-  local res = accept_word()
-  vim.api.nvim_feedkeys(res, 'n', false)
-end
-
-local function accept_line_with_indent()
-  local accept_line = vim.fn['copilot#AcceptLine']
-  assert(accept_line, 'copilot#AcceptLine not found')
-  local res = accept_line()
-  res = res .. '\r'
-  vim.api.nvim_feedkeys(res, 'n', false)
-end
-
-local function accept_until_char()
-  local char = vim.fn.nr2char(vim.fn.getchar())
-
-  local suggestion = vim.fn['copilot#GetDisplayedSuggestion']()
-  local clear_copilot = vim.fn['copilot#Clear']
-  assert(clear_copilot, 'copilot#Clear not found')
-
-  local text = suggestion.text
-  local index = string.find(text, char, 1, true)
-  if index then
-    local partial = string.sub(text, 1, index)
-    vim.api.nvim_feedkeys(partial, 'n', false)
-    clear_copilot()
-  end
-end
 
 local parse_suggestion = function(text, char)
   local matches = {}
@@ -134,24 +89,67 @@ local function highlight_jump_accept()
   jump_from_user_choice(labels, ns, text)
 end
 
-local function accept_line()
-  local accept_line = vim.fn['copilot#AcceptLine']
-  assert(accept_line, 'copilot#AcceptLine not found')
-  local res = accept_line()
-  vim.api.nvim_feedkeys(res, 'n', false)
-end
-
 return {
   'github/copilot.vim',
   config = function()
     vim.g.copilot_no_tab_map = true
-    map('i', '<C-l>', accept_line, { expr = true, silent = true, desc = 'Accept Copilot Line' })
-    map('i', '<M-f>', accept_word, { expr = true, silent = true, desc = 'Accept Copilot Word' })
-    map('i', '<D-a>', accept_until_char, { silent = true, desc = 'Accept Copilot until char' })
-    map('i', '<D-s>', highlight_jump_accept, { silent = true, desc = 'Accept Copilot and jump' })
 
+    local function accept_until_char()
+      local char = vim.fn.nr2char(vim.fn.getchar())
+      local suggestion = vim.fn['copilot#GetDisplayedSuggestion']()
+      local text = suggestion.text
+      local index = string.find(text, char, 1, true)
+      if index then
+        local partial = string.sub(text, 1, index)
+        vim.api.nvim_feedkeys(partial, 'n', false)
+      end
+    end
+
+    local function accept()
+      local accept = vim.fn['copilot#Accept']
+      assert(accept, 'copilot#Accept not found')
+      local res = accept()
+      vim.api.nvim_feedkeys(res, 'n', false)
+    end
+
+    local function accept_with_indent()
+      local accept = vim.fn['copilot#Accept']
+      assert(accept, 'copilot#Accept not found')
+      local res = accept()
+      res = res .. '\r'
+      vim.api.nvim_feedkeys(res, 'n', false)
+    end
+
+    local function accept_word()
+      local accept_word = vim.fn['copilot#AcceptWord']
+      assert(accept_word, 'copilot#AcceptWord not found')
+      local res = accept_word()
+      vim.api.nvim_feedkeys(res, 'n', false)
+    end
+
+    local function accept_line()
+      local accept_line = vim.fn['copilot#AcceptLine']
+      assert(accept_line, 'copilot#AcceptLine not found')
+      local res = accept_line()
+      vim.api.nvim_feedkeys(res, 'n', false)
+    end
+
+    local function accept_line_with_indent()
+      local accept_line = vim.fn['copilot#AcceptLine']
+      assert(accept_line, 'copilot#AcceptLine not found')
+      local res = accept_line()
+      res = res .. '\r'
+      vim.api.nvim_feedkeys(res, 'n', false)
+    end
+
+    map('i', '<C-l>', accept_line, { expr = true, silent = true, desc = 'Accept Copilot Line' })
     map('i', '<M-y>', accept, { expr = true, silent = true, desc = 'Accept Copilot' })
     map('i', '<C-;>', accept_line_with_indent, { expr = true, silent = true, desc = 'Accept Copilot Line' })
     map('i', '<M-;>', accept_with_indent, { expr = true, silent = true, desc = 'Accept Copilot with newline' })
+    map('i', '<M-f>', accept_word, { expr = true, silent = true, desc = 'Accept Copilot Word' })
+
+    -- === customized behavior ===
+    map('i', '<D-a>', accept_until_char, { silent = true, desc = 'Accept Copilot until char' })
+    map('i', '<D-s>', highlight_jump_accept, { silent = true, desc = 'Accept Copilot and jump' })
   end,
 }
