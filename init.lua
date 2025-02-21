f8d82ffc4286b64a67cac78ead05623728815877
Searidang Pa <dangsyncpa@gmail.com>
2025-02-21 16:08:45 -0500

    Save progress:

--- init.lua
+++ init.lua
@@ -5,32 +5,6 @@ function R(name)
   require('plenary.reload').reload_module(name)
 end
 
-vim.g.have_nerd_font = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
-vim.opt.showmode = false
-vim.opt.breakindent = true
-vim.opt.undofile = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.inccommand = 'split' -- Preview substitutions live, as you type!
-vim.opt.cursorline = true
-vim.opt.scrolloff = 10
-
--- copilot plugin config
-vim.g.copilot_no_tab_map = true
-
--- undo tree plugin config
-vim.g.undotree_WindowLayout = 2
-
 vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
 end)
