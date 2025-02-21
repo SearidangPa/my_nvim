e4d9f10b0532e6f243451061ea1a2efbea006650
Searidang Pa <dangsyncpa@gmail.com>
2025-02-21 16:20:06 -0500

    Save progress:

--- set.lua
+++ set.lua
@@ -1,25 +1,35 @@
-vim.g.have_nerd_font = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
-vim.opt.showmode = false
-vim.opt.breakindent = true
-vim.opt.undofile = true
+--- === case insensitive search, unless capital letter is used ===
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
-vim.opt.updatetime = 250
+
+-- === Time Periods ===
 vim.opt.timeoutlen = 300
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+vim.opt.updatetime = 50
+
+-- === Files ===
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
+-- === UI ===
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.inccommand = 'split' -- Preview substitutions live, as you type!
-vim.opt.cursorline = true
 vim.opt.scrolloff = 10
+vim.opt.inccommand = 'nosplit' -- Preview substitutions live, as you type!
 
 -- copilot plugin config
 vim.g.copilot_no_tab_map = true
 
 -- undo tree plugin config
 vim.g.undotree_WindowLayout = 2
+
+-- === Duh ===
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.mouse = 'a'
+vim.g.have_nerd_font = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = 'yes'
+vim.opt.cursorline = true
+vim.opt.breakindent = true
