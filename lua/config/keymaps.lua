c29ac1301c6677fb0bda44cefcb3f89437064b16
Searidang Pa <dangsyncpa@gmail.com>
2025-02-23 22:30:24 -0500

    Save progress:

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -157,4 +157,6 @@ vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
 vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
 vim.keymap.set('n', '<leader>gs', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+-- :let @+ = expand('%:p')
+vim.keymap.set('n', '<leader>cp', [[:let @+ = expand('%:p')<CR>]], { desc = 'Copy file path to clipboard' })
 return {}
