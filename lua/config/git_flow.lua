a13af84e2c9be848feb8a7b1d16e7419508100bd
Searidang Pa <dangsyncpa@gmail.com>
2025-02-26 19:44:27 -0500

    Man is tormented by no greater anxiety than to find an individual consciousness. -- Fyodor Dostoevsky

--- lua/config/git_flow.lua
+++ lua/config/git_flow.lua
@@ -10,6 +10,16 @@ local col = math.floor((vim.o.columns / 5))
 
 local default_no_more_input = {
   'Done with what I set out to do',
+
+  [[Man only likes to count his troubles; he doesn't calculate his joys. -- Fyodor Dostoevsky]],
+  'Beauty will save the world. -- Fyodor Dostoevsky',
+  'To live without hope is to cease to live.-- Fyodor Dostoevsky',
+  'Taking a new step, uttering a new word, is what people fear most. -- Fyodor Dostoevsky',
+  'Man is sometimes extraordinarily, passionately, in love with suffering. -- Fyodor Dostoevsky',
+  'The darker the night, the brighter the stars, the deeper the grief, the closer is God! -- Fyodor Dostoevsky',
+  'Man is tormented by no greater anxiety than to find an individual consciousness. -- Fyodor Dostoevsky',
+  'What is hell? I maintain that it is the suffering of being unable to love. -- Fyodor Dostoevsky',
+  'The cleverest of all, in my opinion, is the man who calls himself a fool at least once a month. -- Fyodor Dostoevsky',
 }
 
 local item_options = {
