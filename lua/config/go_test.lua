f078afdaa5f8cdf09e7781257304a8a997e7d7ba
Searidang Pa <dangsyncpa@gmail.com>
2025-03-05 16:05:37 -0500

    ```lua --- Renamed util_find_func.lua functions to avoid conflict with new Get_enclosing_fn_info and Get_enclosing_test
    function Nearest_func_node()
      local ts_utils = require('nvim-treesitter.utils')
      local parser = vim.treesitter.get_parser(vim.api.nvim_get_current_buf())
      local tree = parser:parse()[1]
      local cursor_pos = vim.fn.col('.') - 1
      local node = tree:nodes_for_cursor_position(cursor_pos, cursor_pos)
    
      while node do
        if node:type() == 'function_declaration' then
          return node
        end
        node = node:parent()
      end
    
      return nil
    end
    ```

--- lua/config/go_test.lua
+++ lua/config/go_test.lua
@@ -1,44 +1,10 @@
 local M = {}
 require 'config.util_find_func'
-require 'config.util_go_test_on_save'
 
 local ts_utils = require 'nvim-treesitter.ts_utils'
 local mini_notify = require 'mini.notify'
 local make_notify = mini_notify.make_notify {}
 local get_node_text = vim.treesitter.get_node_text
-
-function Get_enclosing_fn_info()
-  local node = ts_utils.get_node_at_cursor()
-  while node do
-    if node:type() ~= 'function_declaration' then
-      node = node:parent() -- Traverse up the node tree to find a function node
-      goto continue
-    end
-
-    local func_name_node = node:child(1)
-    if func_name_node then
-      local func_name = get_node_text(func_name_node, 0)
-      local startLine, _, _ = node:start()
-      return startLine + 1, func_name -- +1 to convert 0-based to 1-based lua indexing system
-    end
-    ::continue::
-  end
-
-  return nil
-end
-
-function Get_enclosing_test()
-  local _, testName = Get_enclosing_fn_info()
-  if not testName then
-    print 'Not in a function'
-    return nil
-  end
-  if not string.match(testName, 'Test_') then
-    print(string.format('Not in a test function: %s', testName))
-    return nil
-  end
-  return testName
-end
 
 function Clean_up_prev_job(job_id)
   if job_id ~= -1 then
--- lua/config/util_find_func.lua
+++ lua/config/util_find_func.lua
@@ -137,7 +137,37 @@ function Nearest_func_node()
   return func_node
 end
 
+function Get_enclosing_fn_info()
+  local node = ts_utils.get_node_at_cursor()
+  while node do
+    if node:type() ~= 'function_declaration' then
+      node = node:parent() -- Traverse up the node tree to find a function node
+      goto continue
+    end
 
+    local func_name_node = node:child(1)
+    if func_name_node then
+      local func_name = get_node_text(func_name_node, 0)
+      local startLine, _, _ = node:start()
+      return startLine + 1, func_name -- +1 to convert 0-based to 1-based lua indexing system
+    end
+    ::continue::
+  end
 
+  return nil
+end
+
+function Get_enclosing_test()
+  local _, testName = Get_enclosing_fn_info()
+  if not testName then
+    print 'Not in a function'
+    return nil
+  end
+  if not string.match(testName, 'Test_') then
+    print(string.format('Not in a test function: %s', testName))
+    return nil
+  end
+  return testName
+end
 
 
