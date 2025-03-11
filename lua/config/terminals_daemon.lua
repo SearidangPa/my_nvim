2d5de62b824c1124f6caad3b6cbf3f6f312ae665
Searidang Pa <dangsyncpa@gmail.com>
2025-03-11 10:52:19 -0400

    Added `GhActionsCheckRetry`, `GhActionsRetryTillSuccess`, and `GhActionsStatus` commands for GitHub Actions checks 🚀 Exposed `exec_command` function to run commands in a floating terminal 💻

--- lua/config/terminals_daemon.lua
+++ lua/config/terminals_daemon.lua
@@ -3,7 +3,7 @@ local make_notify = require('mini.notify').make_notify {}
 local terminal_multiplexer = require('config.terminal_multiplexer').new()
 M.terminal_multiplexer = terminal_multiplexer
 
-local exec_command = function(command, title)
+M.exec_command = function(command, title)
   terminal_multiplexer:toggle_float_terminal(title)
   local current_float_term_state = terminal_multiplexer:toggle_float_terminal(title)
   assert(current_float_term_state, 'Failed to toggle float terminal')
@@ -18,17 +18,27 @@ end
 
 local function run_drive()
   if vim.fn.has 'win32' == 1 then
-    exec_command('dr;rds\r', 'drive')
+    M.exec_command('dr;rds\r', 'drive')
   else
-    exec_command('dr && kill_port_4420 && ./bin/client --stdout --onlyUserIDs', 'drive')
+    M.exec_command('dr && kill_port_4420 && ./bin/client --stdout --onlyUserIDs', 'drive')
   end
 end
 
--- === Commands and keymaps ===
+-- === coding ===
 vim.api.nvim_create_user_command('RunDrive', run_drive, {})
-vim.api.nvim_create_user_command('RunCloudDrive', function() exec_command('m; std', 'cloud drive') end, {})
+vim.api.nvim_create_user_command('RunCloudDrive', function() M.exec_command('m; std', 'cloud drive') end, {})
 vim.keymap.set('n', '<leader>dr', run_drive, { silent = true, desc = '[D]aemon [R]un drive' })
-vim.keymap.set('n', '<leader>dc', function() exec_command('m; std', 'cloud drive') end, { desc = '[D]aemon [C]loud drive' })
+vim.keymap.set('n', '<leader>dc', function() M.exec_command('m; std', 'cloud drive') end, { desc = '[D]aemon [C]loud drive' })
 vim.keymap.set('n', '<leader>sd', function() terminal_multiplexer:select_terminal() end, { desc = '[S]earch [D]aemon terminals' })
 
+-- === gh_actions ===
+local daemon_terminal_name = 'gh actions'
+vim.api.nvim_create_user_command('GhActionsCheckRetry', function() M.exec_command('gh-check-with-rerun', daemon_terminal_name) end, {})
+
+vim.api.nvim_create_user_command('GhActionsRetryTillSuccess', function()
+  local interval_minutes = 10
+  M.exec_command(string.format('retry_until_success %d', interval_minutes), daemon_terminal_name)
+end, {})
+
+vim.api.nvim_create_user_command('GhActionsStatus', function() terminal_multiplexer:toggle_float_terminal(daemon_terminal_name) end, {})
 return M
