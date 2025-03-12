9a2d739771ff9c996d973ff648d6850e461a3e0c
Searidang Pa <dangsyncpa@gmail.com>
2025-03-12 12:10:31 -0400

    Added new keybinding to add files at the back of Harpoon list 🚀✨

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,6 +9,7 @@ return {
     local map = vim.keymap.set
 
     map('n', '<localleader>a', function() harpoon:list():prepend() end, { desc = 'harpoon [A]dd' })
+    map('n', '<localleader>b', function() harpoon:list():add() end, { desc = 'harpoon add at the [B]ack' })
     map('n', '<localleader>l', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'harpoon [L]ist' })
     map('n', '<C-S-P>', function() harpoon:list():prev() end)
     map('n', '<C-S-N>', function() harpoon:list():next() end)
