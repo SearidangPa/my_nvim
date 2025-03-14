9c3aa2bcf57b0bff86bd88671ca8a77cc0aab62f
Searidang Pa <dangsyncpa@gmail.com>
2025-03-14 10:45:29 -0400

    Update `test_tracker` reference in `lualine.lua` 🚀 Fix incorrect access to `test_tracker` variable 👍

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -65,7 +65,7 @@ end
 
 local function tracked_tests_list()
   local terminal_tests = require 'config.terminals_test'
-  local test_tracker = terminal_tests.get_test_tracker()
+  local test_tracker = terminal_tests.test_tracker
   local list_tests_names = ''
   for test_name, _ in pairs(test_tracker) do
     list_tests_names = list_tests_names .. ' | ' .. test_name
