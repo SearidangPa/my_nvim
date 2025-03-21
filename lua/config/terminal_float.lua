512536d24261271bf402eee7220974caf527dba3
Searidang Pa <dangsyncpa@gmail.com>
2025-03-21 07:02:02 -0400

    Beauty will save the world. -- Fyodor Dostoevsky

--- lua/config/terminal_float.lua
+++ lua/config/terminal_float.lua
@@ -4,6 +4,8 @@ local nui_input = require 'nui.input'
 local event = require('nui.utils.autocmd').event
 local terminal_multiplexer = require('config.terminal_multiplexer').new()
 local terminal_name = 'terminal'
+M = {}
+M.float_terminal_state = nil
 
 local choice_options_unix = {
   'gh_rerun_failed',
@@ -42,6 +44,22 @@ local function Send_command_toggle_term()
   end)
 end
 
-vim.keymap.set({ 't', 'n' }, '<localleader>tt', function() terminal_multiplexer:toggle_float_terminal(terminal_name) end, { desc = '[T]erminal [T]oggle' })
+local function toggle_float_terminal_with_bind()
+  M.float_terminal_state = terminal_multiplexer:toggle_float_terminal(terminal_name)
+  assert(M.float_terminal_state, 'Failed to toggle float terminal')
+
+  local close_term = function()
+    if vim.api.nvim_win_is_valid(M.float_terminal_state.footer_win) then
+      vim.api.nvim_win_hide(M.float_terminal_state.footer_win)
+    end
+    if vim.api.nvim_win_is_valid(M.float_terminal_state.win) then
+      vim.api.nvim_win_hide(M.float_terminal_state.win)
+    end
+  end
+  vim.keymap.set('n', 'q', close_term, { buffer = M.float_terminal_state.buf })
+end
+
+vim.keymap.set({ 't', 'n' }, '<localleader>tt', toggle_float_terminal_with_bind, { desc = '[T]erminal [T]oggle' })
 vim.keymap.set('n', '<localleader>ts', Send_command_toggle_term, { desc = '[T]erminal [S]end command' })
-return {}
+
+return M
