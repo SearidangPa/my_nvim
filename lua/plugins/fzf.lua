0313673918358417f8238206bfe959d7a5023d46
Searidang Pa <dangsyncpa@gmail.com>
2025-03-29 12:47:36 -0400

    sa

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -2,16 +2,6 @@ return {
   'ibhagwan/fzf-lua',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    vim.keymap.set('n', '<leader>sb', function()
-      require('fzf-lua').git_branches {
-        -- cmd = 'git branch --all --color',
-        -- actions = {
-        --   ['default'] = function(selected)
-        --     local branch = selected[1]:match '([^%s]+)$'
-        --     vim.cmd('!git switch ' .. branch)
-        --   end,
-        -- },
-      }
-    end, { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>sb', function() require('fzf-lua').git_branches {} end, { noremap = true, silent = true })
   end,
 }
