e335827fd1a9e1da76938fbc3874b5804b2b1469
Searidang Pa <dangsyncpa@gmail.com>
2025-03-29 12:44:53 -0400

    Save progress

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -4,13 +4,13 @@ return {
   config = function()
     vim.keymap.set('n', '<leader>sb', function()
       require('fzf-lua').git_branches {
-        cmd = 'git branch --all --color',
-        actions = {
-          ['default'] = function(selected)
-            local branch = selected[1]:match '([^%s]+)$'
-            vim.cmd('!git switch ' .. branch)
-          end,
-        },
+        -- cmd = 'git branch --all --color',
+        -- actions = {
+        --   ['default'] = function(selected)
+        --     local branch = selected[1]:match '([^%s]+)$'
+        --     vim.cmd('!git switch ' .. branch)
+        --   end,
+        -- },
       }
     end, { noremap = true, silent = true })
   end,
