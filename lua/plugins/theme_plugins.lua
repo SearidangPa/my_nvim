16bd1e4335530d7a32e092b6dfa12b2a65cb2b73
SearidangPa <dangsyncpa@gmail.com>
2025-03-29 12:12:03 -0400

    Save progress

--- lua/plugins/theme_plugins.lua
+++ lua/plugins/theme_plugins.lua
@@ -2,7 +2,16 @@ return {
   {
     'rebelot/kanagawa.nvim',
   },
-  { 'rose-pine/neovim', name = 'rose-pine' },
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    config = function()
+      require('rose-pine').setup {
+        variant = 'moon',
+        disable_italics = true,
+      }
+    end,
+  },
   {
     'catppuccin/nvim',
     name = 'catppuccin',
