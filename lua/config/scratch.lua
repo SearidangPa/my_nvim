77f6b8c9540018c6b5fd579393f7068616a883f7
Searidang Pa <dangsyncpa@gmail.com>
2025-03-30 15:39:57 -0400

    Save progress

--- lua/config/scratch.lua
+++ lua/config/scratch.lua
@@ -1,10 +1,29 @@
 local M = {}
 
-local function new_scratch_buf()
+M.new_prompt_pr_desc = function()
+  -- Create new scratch buffer
+  vim.cmd 'enew'
+  vim.cmd 'setlocal buftype=nofile bufhidden=hide noswapfile'
+  vim.cmd 'setlocal filetype=markdown'
+
+  -- Execute the ai_pr_desc function and capture its output
+  local output = vim.fn.system 'zsh -c "source ~/.zshrc && ai_pr_desc"'
+
+  -- Insert the output at the beginning of the buffer
+  local lines = vim.split(output, '\n')
+  vim.api.nvim_buf_set_lines(0, 0, 0, false, lines)
+
+  -- Move cursor to the beginning of the buffer
+  vim.cmd 'normal! gg'
+end
+
+vim.api.nvim_create_user_command('NewPromptPRDesc', M.new_prompt_pr_desc, {})
+
+M.new_scratch_buf = function()
   vim.cmd 'enew'
   vim.cmd 'setlocal buftype=nofile bufhidden=hide noswapfile'
   vim.cmd 'setlocal filetype=markdown'
 end
+vim.api.nvim_create_user_command('NewScratch', M.new_scratch_buf, { desc = 'Start a scratch buffer' })
 
-vim.api.nvim_create_user_command('NewScratch', new_scratch_buf, { desc = 'Start a scratch buffer' })
 return M
