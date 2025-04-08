0473e19d4b193c1ab674814b24dc273cd29ae6c9
Searidang Pa <dangsyncpa@gmail.com>
2025-04-07 22:34:45 -0400

    Save progress

--- lua/plugins/copilot_hop.lua
+++ lua/plugins/copilot_hop.lua
@@ -1,10 +1,10 @@
 return {
-  'SearidangPa/copilot_hop.nvim',
+  'SearidangPa/hop_copilot.nvim',
   dependencies = {
     'github/copilot.vim',
   },
   config = function()
-    vim.keymap.set('i', '<M-s>', require('copilot_hop').copilot_hop, { silent = true, desc = 'copilot_hop' })
-    vim.keymap.set('i', '<D-s>', require('copilot_hop').copilot_hop, { silent = true, desc = 'copilot_hop' })
+    vim.keymap.set('i', '<M-s>', require('hop_copilot').hop_copilot, { silent = true, desc = 'hop copilot' })
+    vim.keymap.set('i', '<D-s>', require('hop_copilot').hop_copilot, { silent = true, desc = 'hop copilot' })
   end,
 }
