bef556e1564500bcf67ff827d14a7a53247c4c79
Searidang Pa <dangsyncpa@gmail.com>
2025-04-09 08:53:09 -0400

    Save progress

--- lua/plugins/test-tt.lua
+++ lua/plugins/test-tt.lua
@@ -1,3 +1,34 @@
+local terminal_test_set_up = function()
+  local go_test_tt = require 'go-test-tt'
+  vim.api.nvim_create_user_command('TerimanlTest', function()
+    local test_command_format = 'go test ./... -v -run %s'
+    go_test_tt.test_nearest_in_terminal(test_command_format)
+  end, {})
+
+  vim.api.nvim_create_user_command('TerimanlTestBuf', function()
+    local test_command_format = 'go test ./... -v -run %s'
+    go_test_tt.test_buf_in_terminals(test_command_format)
+  end, {})
+
+  vim.api.nvim_create_user_command('TerminalTestIntegration', function()
+    local test_command_format
+    if vim.fn.has 'win32' == 1 then
+      test_command_format = 'gitBash -c "go test integration_tests/*.go -v -run %s"\r'
+    else
+      test_command_format = 'go test integration_tests/*.go -v -run %s'
+    end
+    go_test_tt.test_nearest_in_terminal(test_command_format)
+  end, {})
+
+  vim.api.nvim_create_user_command('TerminalTestSetModeStaging', function()
+    vim.env.MODE, vim.env.UKS = 'staging', 'others'
+  end, {})
+
+  vim.api.nvim_create_user_command('TerminalTestSetModeDev', function()
+    vim.env.MODE, vim.env.UKS = 'dev', 'others'
+  end, {})
+end
+
 return {
   'SearidangPa/go-test-tt.nvim',
   dependencies = {
@@ -6,33 +37,6 @@ return {
   config = function()
     local go_test_tt = require 'go-test-tt'
     go_test_tt.setup()
-
-    vim.api.nvim_create_user_command('TerimanlTest', function()
-      local test_command_format = 'go test ./... -v -run %s'
-      go_test_tt.test_nearest_in_terminal(test_command_format)
-    end, {})
-
-    vim.api.nvim_create_user_command('TerimanlTestBuf', function()
-      local test_command_format = 'go test ./... -v -run %s'
-      go_test_tt.test_buf_in_terminals(test_command_format)
-    end, {})
-
-    vim.api.nvim_create_user_command('TerminalTestIntegration', function()
-      local test_command_format
-      if vim.fn.has 'win32' == 1 then
-        test_command_format = 'gitBash -c "go test integration_tests/*.go -v -run %s"\r'
-      else
-        test_command_format = 'go test integration_tests/*.go -v -run %s'
-      end
-      go_test_tt.test_nearest_in_terminal(test_command_format)
-    end, {})
-
-    vim.api.nvim_create_user_command('TerminalTestSetModeStaging', function()
-      vim.env.MODE, vim.env.UKS = 'staging', 'others'
-    end, {})
-
-    vim.api.nvim_create_user_command('TerminalTestSetModeDev', function()
-      vim.env.MODE, vim.env.UKS = 'dev', 'others'
-    end, {})
+    terminal_test_set_up()
   end,
 }
