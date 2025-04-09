eec5577846299559c17f4b2bbfec68de491cc479
Searidang Pa <dangsyncpa@gmail.com>
2025-04-09 17:06:39 -0400

    Save progress

--- lua/config/git_flow.lua
+++ lua/config/git_flow.lua
@@ -128,8 +128,6 @@ local function push_all()
     git_push()
     async_make_job.make_lint()
     async_make_job.make_all()
-    local test_t = require 'go-test-tt'
-    test_t.test_tracked_in_terminal()
   end
   git_add_all(function() select_commit_message_prompt(cb) end)
 end
