8c4db1b5832efb7af742543047b7b09275817e0e
Searidang Pa <dangsyncpa@gmail.com>
2025-04-10 08:24:59 -0400

    Removed mini.notify setup and associated command from lua/plugins/mini.lua.

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,9 +2,6 @@ return { -- Collection of various small independent plugins/modules
   'echasnovski/mini.nvim',
   config = function()
     require('mini.ai').setup { n_lines = 500 }
-    require('mini.notify').setup {}
     require('mini.icons').setup {}
-    local notify = require 'mini.notify'
-    vim.api.nvim_create_user_command('ClearNotify', notify.clear, {})
   end,
 }
