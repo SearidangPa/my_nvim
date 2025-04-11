ac78051997bb2fcb51e56b92a4c66905450634e9
SearidangPa <dangsyncpa@gmail.com>
2025-04-11 14:29:27 -0400

    Done with what I set out to do

--- lua/plugins/go-test-tt.lua
+++ lua/plugins/go-test-tt.lua
@@ -1,4 +1,5 @@
 local map = vim.keymap.set
+
 M = {
   'SearidangPa/go-test-tt.nvim',
   dependencies = {
@@ -41,6 +42,8 @@ M._terminal_test_set_up = function(integration_test_command_format)
   end, {})
 
   vim.api.nvim_create_user_command('TerminalIntegrationTest', function() terminal_test.test_nearest_in_terminal(integration_test_command_format) end, {})
+  map('n', '<leader>G', ':TerminalIntegrationTest<CR>', { desc = '[G]o test in terminal' })
+  map('n', '<leader>T', ':TerminalTestToggleView<CR>', { desc = '[G]o test in terminal' })
 
   vim.api.nvim_create_user_command('TerminalIntegrationTestBuf', function() terminal_test.test_buf_in_terminals(integration_test_command_format) end, {})
   vim.api.nvim_create_user_command('GoTestSetModeDev', function()
