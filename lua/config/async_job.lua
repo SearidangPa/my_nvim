98cd03f3c1f47224213a0316c05ce6ad9356740d
Searidang Pa <dangsyncpa@gmail.com>
2025-04-12 08:43:21 -0400

    Removed unused `fidget_name` parameter from `create_fidget_handle` in `async_job.lua`.

--- lua/config/async_job.lua
+++ lua/config/async_job.lua
@@ -1,10 +1,8 @@
 local async_job = {}
 
 ---@param cmd string
----@param fidget_name string
-local function create_fidget_handle(cmd, fidget_name)
+local function create_fidget_handle(cmd)
   assert(cmd, 'cmd is required')
-  assert(fidget_name, 'fidget_name is required')
   local fidget_title
   if type(cmd) == 'table' then
     fidget_title = table.concat(cmd, ' ')
@@ -13,9 +11,8 @@ local function create_fidget_handle(cmd, fidget_name)
   end
   local fidget = require 'fidget'
   return fidget.progress.handle.create {
-    title = fidget_title,
     lsp_client = {
-      name = fidget_name,
+      name = cmd,
     },
   }
 end
@@ -23,21 +20,21 @@ end
 async_job.make_all = function()
   local cmd = 'make -j all'
   local make_all_ns = vim.api.nvim_create_namespace 'make_all'
-  local fidget_handle = create_fidget_handle(cmd, 'make_all')
+  local fidget_handle = create_fidget_handle(cmd)
   require('config.util_job').start_job { cmd = cmd, fidget_handle = fidget_handle, ns = make_all_ns }
 end
 
 async_job.make_lint = function()
   local cmd = 'make -j lint'
   local linter_ns = vim.api.nvim_create_namespace 'linter'
-  local fidget_handle = create_fidget_handle(cmd, 'linter')
+  local fidget_handle = create_fidget_handle(cmd)
   require('config.util_job').start_job { cmd = cmd, fidget_handle = fidget_handle, ns = linter_ns }
 end
 
 async_job.go_mod_tidy = function()
   local cmd = 'go mod tidy'
   local go_mod_tidy_ns = vim.api.nvim_create_namespace 'go_mod_tidy'
-  local fidget_handle = create_fidget_handle(cmd, 'go_mod_tidy')
+  local fidget_handle = create_fidget_handle(cmd)
   require('config.util_job').start_job { cmd = cmd, fidget_handle = fidget_handle, ns = go_mod_tidy_ns }
 end
 
