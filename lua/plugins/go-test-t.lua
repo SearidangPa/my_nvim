67bbe47580ea5364109c9947ebc1b28a454bda10
Searidang Pa <dangsyncpa@gmail.com>
2025-04-13 17:13:34 -0400

    Add new user command `TermIntegrationTestView` for viewing enclosing test terminal in Go integration tests.

--- lua/plugins/go-test-t.lua
+++ lua/plugins/go-test-t.lua
@@ -30,6 +30,7 @@ function M._integration_test_set_up()
   local term_test = terminal_test.new { test_command_format = M._get_integration_test_command_format() }
   vim.api.nvim_create_user_command('TermIntegrationTest', function() term_test:test_nearest_in_terminal() end, {})
   vim.api.nvim_create_user_command('TermIntegrationTestBuf', function() term_test:test_buf_in_terminals() end, {})
+  vim.api.nvim_create_user_command('TermIntegrationTestView', function() term_test:view_enclosing_test_terminal() end, {})
   vim.env.MODE, vim.env.UKS = 'staging', 'others'
   vim.api.nvim_create_user_command('SetModeDev', function()
     vim.env.MODE, vim.env.UKS = 'dev', 'others'
