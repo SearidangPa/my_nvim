106a954c9fb4c7f42a9f479182107d6c5eca48e0
Searidang Pa <dangsyncpa@gmail.com>
2025-04-13 20:04:32 -0400

    Updated Go test plugin commands and removed unused integration test setup.

--- lua/plugins/go-test-t.lua
+++ lua/plugins/go-test-t.lua
@@ -9,14 +9,6 @@ M = {
     local go_test_t = require 'go-test-t'
     local go_tester = go_test_t.new {}
     M._setup_user_commands(go_tester, 'GoTest')
-
-    M._integration_test_set_up()
-
-    -- ---@type TerminalTestTracker
-    -- local tracker = require 'terminal_test.tracker'
-    -- local map = vim.keymap.set
-    -- map('n', '<leader>tr', tracker.toggle_tracker_window, { desc = '[A]dd [T]est to tracker' })
-    -- map('n', '<leader>at', function() tracker.add_test_to_tracker 'go test ./... -v -run %s' end, { desc = '[A]dd [T]est to tracker' })
   end,
 }
 
@@ -24,15 +16,22 @@ M = {
 ---@param user_command_prefix string
 function M._setup_user_commands(go_tester, user_command_prefix)
   local term_tester = go_tester.term_tester
-  vim.api.nvim_create_user_command(user_command_prefix .. 'All', function() term_tester:test_nearest_in_terminal() end, {})
+  vim.api.nvim_create_user_command(user_command_prefix .. 'All', function() go_tester:run_test_all() end, {})
+  vim.api.nvim_create_user_command(user_command_prefix .. 'ToggleDisplay', function() go_tester:toggle_display() end, {})
+  vim.api.nvim_create_user_command(user_command_prefix .. 'LoadQuackTestQuickfix', function() go_tester:load_quack_tests() end, {})
+
   vim.api.nvim_create_user_command(user_command_prefix .. 'Term', function() term_tester:test_nearest_in_terminal() end, {})
   vim.api.nvim_create_user_command(user_command_prefix .. 'TermBuf', function() term_tester:test_buf_in_terminals() end, {})
   vim.api.nvim_create_user_command(user_command_prefix .. 'TermView', function() term_tester:view_enclosing_test_terminal() end, {})
   vim.api.nvim_create_user_command(user_command_prefix .. 'TermSearch', function() term_tester.terminals:search_terminal() end, {})
   vim.api.nvim_create_user_command(user_command_prefix .. 'TermViewLast', function() term_tester:view_last_test_terminal() end, {})
-  vim.api.nvim_create_user_command(user_command_prefix .. 'TermToggleDisplay', function() term_tester.displayer:toggle_display() end, {})
-  vim.api.nvim_create_user_command(user_command_prefix .. 'ToggleDisplay', function() go_tester:toggle_display() end, {})
-  vim.api.nvim_create_user_command(user_command_prefix .. 'LoadQuackTestQuickfix', function() go_tester:load_quack_tests() end, {})
+  vim.api.nvim_create_user_command(user_command_prefix .. 'TermToggleDisplay', function() term_tester.term_test_displayer:toggle_display() end, {})
+
+  -- ---@type TerminalTestTracker
+  -- local tracker = require 'terminal_test.tracker'
+  -- local map = vim.keymap.set
+  -- map('n', '<leader>tr', tracker.toggle_tracker_window, { desc = '[A]dd [T]est to tracker' })
+  -- map('n', '<leader>at', function() tracker.add_test_to_tracker 'go test ./... -v -run %s' end, { desc = '[A]dd [T]est to tracker' })
 end
 
 function M._integration_test_set_up()
