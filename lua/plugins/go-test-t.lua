49d560e5b6cfd8b0dc7c645d66d3a66923a3f429
Searidang Pa <dangsyncpa@gmail.com>
2025-04-13 22:39:12 -0400

    Update `go-test-t.lua` to remove environment variables from test commands, change user command prefix to 'Go', and update keymap description.

--- lua/plugins/go-test-t.lua
+++ lua/plugins/go-test-t.lua
@@ -7,37 +7,33 @@ M = {
     'j-hui/fidget.nvim',
   },
   config = function()
-    ---@type GoTestT
-    local go_tester = require 'go-test-t'
-    go_tester.new {}
-    map('n', '<leader>T', ':TestTermView<CR>', { desc = 'Test: View enclosing test terminal' })
+    vim.env.UKS = 'others'
+    vim.env.MODE = 'staging'
 
-    M._integration_test_set_up()
+    local function term_test_command_format()
+      if vim.fn.has 'win32' == 1 then
+        return 'go test .\\integration_tests\\ -v -run %s\r'
+      else
+        return 'go test integration_tests/*.go -v -run %s\r'
+      end
+    end
+
+    local function test_command()
+      if vim.fn.has 'win32' == 1 then
+        return 'go test .\\integration_tests\\ -v \r'
+      else
+        return 'go test integration_tests/*.go -v \r'
+      end
+    end
+
+    require('go-test-t').new {
+      test_command = test_command(),
+      term_test_command_format = term_test_command_format(),
+      user_command_prefix = 'Go',
+    }
+
+    map('n', '<leader>T', ':GoTestTermView<CR>', { desc = 'Test: View enclosing test terminal' })
   end,
 }
-
-function M._integration_test_set_up()
-  local function term_test_command_format()
-    if vim.fn.has 'win32' == 1 then
-      return 'MODE=staging UKS=others go test .\\integration_tests\\ -v -run %s\r'
-    else
-      return 'MODE=staging UKS=others go test integration_tests/*.go -v -run %s\r'
-    end
-  end
-
-  local function test_command()
-    if vim.fn.has 'win32' == 1 then
-      return 'MODE=staging UKS=others go test .\\integration_tests\\ -v \r'
-    else
-      return 'MODE=staging UKS=others go test integration_tests/*.go -v \r'
-    end
-  end
-
-  require('go-test-t').new {
-    test_command = test_command(),
-    term_test_command_format = term_test_command_format(),
-    user_command_prefix = 'Integration',
-  }
-end
 
 return M
