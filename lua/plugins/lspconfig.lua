8a62821bb0e0b0a7cb79ea3f5a8abeef912ba279
Searidang Pa <dangsyncpa@gmail.com>
2025-04-18 15:58:38 -0400

    Updated keybindings for LSP actions in telescope integration. Changed 'gd' to open type definitions instead of regular definitions, removed redundant '<leader>D' mapping, and clarified descriptions for better user understanding.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -32,12 +32,11 @@ local function lsp_attach_keybind()
           desc = 'LSP: ' .. desc,
         })
       end
-      map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+      map('gd', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
       map('<localleader>r', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
       map('<localleader>d', require('telescope.builtin').lsp_definitions, '[G]oto [D]eclaration')
 
       map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-      map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
       map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
       map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
