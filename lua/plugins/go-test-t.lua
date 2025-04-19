145a3fbfc925d3602b6c9ed57d78fed248a82438
Searidang Pa <dangsyncpa@gmail.com>
2025-04-19 10:45:43 -0400

    Refactor Go test configuration to dynamically set command prefix based on current working directory.

--- lua/plugins/go-test-t.lua
+++ lua/plugins/go-test-t.lua
@@ -6,33 +6,17 @@ return {
     'SearidangPa/terminal-multiplexer.nvim',
   },
   config = function()
-    local function term_test_command_format()
-      if vim.fn.has 'win32' == 1 then
-        return 'go test .\\integration_tests\\ -v -run %s\r'
-      else
-        return 'go test integration_tests/*.go -v -run %s\r'
-      end
-    end
-
-    local function test_command_json()
-      if vim.fn.has 'win32' == 1 then
-        return 'go test .\\integration_tests\\ -v --json \r'
-      else
-        return 'go test integration_tests/*.go -v --json \r'
-      end
+    local go_test_prefix = 'go test'
+    local cwd = vim.fn.getcwd()
+    if string.match(cwd, 'drive') then
+      go_test_prefix = 'MODE=staging UKS=others go test'
     end
 
     require('go-test-t').new {
-      term_test_command_format = term_test_command_format(),
-      test_command_format_json = test_command_json(),
+      go_test_prefix = go_test_prefix,
       user_command_prefix = 'Go',
     }
-
-    require('go-test-t').new {
-      user_command_prefix = 'Play',
-    }
-
-    vim.keymap.set('n', '<localleader>tt', ':PlayTestTermView<CR>', { desc = 'Test: View enclosing test terminal' })
-    vim.keymap.set('n', '<leader>T', ':PlayTestTerm<CR>', { desc = 'Test: View enclosing test terminal' })
+    vim.keymap.set('n', '<localleader>tt', ':GoTestTermView<CR>', { desc = 'Test: View enclosing test terminal' })
+    vim.keymap.set('n', '<leader>T', ':GoTestTerm<CR>', { desc = 'Test: View enclosing test terminal' })
   end,
 }
