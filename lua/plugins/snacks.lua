6fc1c5599e1a0d56a5f96f8d5de4bae367a4ee0f
Searidang Pa <dangsyncpa@gmail.com>
2025-04-20 22:34:29 -0400

    Update `snacks.lua` to adjust priority, lazy loading, remove unused options, and streamline key bindings.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,53 +1,25 @@
--- return {
---   'folke/snacks.nvim',
---   opts = {
---     bigfile = { enabled = true },
---     indent = { enabled = true },
---     input = { enabled = true },
---     picker = { enabled = true },
---     notifier = { enabled = true },
---     quickfile = { enabled = true },
---     scope = { enabled = true },
---   },
--- }
-
 return {
   'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
   opts = {
-    profiler = { enabled = true },
     bigfile = { enabled = true },
     indent = { enabled = true },
     picker = { enabled = true },
     quickfile = { enabled = true },
-    scope = { enabled = true },
-    words = { enabled = true },
   },
 
   keys = {
-    -- Top Pickers & Explorer
-    { '<leader><space>', function() Snacks.picker.smart() end, desc = 'Smart Find Files' },
-    { '<leader>,', function() Snacks.picker.buffers() end, desc = 'Buffers' },
-    { '<leader>/', function() Snacks.picker.grep() end, desc = 'Grep' },
     { '<leader>:', function() Snacks.picker.command_history() end, desc = 'Command History' },
-
-    -- find
-    { '<leader>fb', function() Snacks.picker.buffers() end, desc = 'Buffers' },
-    { '<leader>fc', function() Snacks.picker.files { cwd = vim.fn.stdpath 'config' } end, desc = 'Find Config File' },
-    { '<leader>ff', function() Snacks.picker.files() end, desc = 'Find Files' },
-    { '<leader>fg', function() Snacks.picker.git_files() end, desc = 'Find Git Files' },
     { '<leader>fp', function() Snacks.picker.projects() end, desc = 'Projects' },
-    { '<leader>fr', function() Snacks.picker.recent() end, desc = 'Recent' },
-    -- git
 
-    { '<leader>gb', function() Snacks.picker.git_branches() end, desc = 'Git Branches' },
+    -- git
+    { '<leader>gf', function() Snacks.picker.git_log_file() end, desc = 'Git Log File' },
     { '<leader>gl', function() Snacks.picker.git_log() end, desc = 'Git Log' },
     { '<leader>gL', function() Snacks.picker.git_log_line() end, desc = 'Git Log Line' },
     { '<leader>gs', function() Snacks.picker.git_status() end, desc = 'Git Status' },
     { '<leader>gS', function() Snacks.picker.git_stash() end, desc = 'Git Stash' },
     { '<leader>gd', function() Snacks.picker.git_diff() end, desc = 'Git Diff (Hunks)' },
-    { '<leader>gf', function() Snacks.picker.git_log_file() end, desc = 'Git Log File' },
     -- Grep
 
     { '<leader>s/', function() Snacks.picker.lines() end, desc = 'Buffer Lines' },
@@ -79,42 +51,15 @@ return {
     -- LSP
     { 'gd', function() Snacks.picker.lsp_definitions() end, desc = 'Goto Definition' },
     { 'gD', function() Snacks.picker.lsp_declarations() end, desc = 'Goto Declaration' },
-    { 'gr', function() Snacks.picker.lsp_references() end, nowait = true, desc = 'References' },
     { 'gI', function() Snacks.picker.lsp_implementations() end, desc = 'Goto Implementation' },
     { 'gy', function() Snacks.picker.lsp_type_definitions() end, desc = 'Goto T[y]pe Definition' },
-    -- Other
-    { '<leader>z', function() Snacks.zen() end, desc = 'Toggle Zen Mode' },
-    { '<leader>Z', function() Snacks.zen.zoom() end, desc = 'Toggle Zoom' },
-    { '<leader>.', function() Snacks.scratch() end, desc = 'Toggle Scratch Buffer' },
-    { '<leader>S', function() Snacks.scratch.select() end, desc = 'Select Scratch Buffer' },
-    { '<leader>n', function() Snacks.notifier.show_history() end, desc = 'Notification History' },
+
     { '<leader>bd', function() Snacks.bufdelete() end, desc = 'Delete Buffer' },
     { '<leader>cR', function() Snacks.rename.rename_file() end, desc = 'Rename File' },
     { '<leader>gB', function() Snacks.gitbrowse() end, desc = 'Git Browse', mode = { 'n', 'v' } },
     { '<leader>gg', function() Snacks.lazygit() end, desc = 'Lazygit' },
-    { '<leader>un', function() Snacks.notifier.hide() end, desc = 'Dismiss All Notifications' },
     { '<c-/>', function() Snacks.terminal() end, desc = 'Toggle Terminal' },
     { '<c-_>', function() Snacks.terminal() end, desc = 'which_key_ignore' },
-    { ']]', function() Snacks.words.jump(vim.v.count1) end, desc = 'Next Reference', mode = { 'n', 't' } },
-    { '[[', function() Snacks.words.jump(-vim.v.count1) end, desc = 'Prev Reference', mode = { 'n', 't' } },
-    {
-      '<leader>N',
-      desc = 'Neovim News',
-      function()
-        Snacks.win {
-          file = vim.api.nvim_get_runtime_file('doc/news.txt', false)[1],
-          width = 0.6,
-          height = 0.6,
-          wo = {
-            spell = false,
-            wrap = false,
-            signcolumn = 'yes',
-            statuscolumn = ' ',
-            conceallevel = 3,
-          },
-        }
-      end,
-    },
   },
 
   init = function()
