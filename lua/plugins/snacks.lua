3cafdf174fd540d5e56c3c382fef4b5bc8042624
Searidang Pa <dangsyncpa@gmail.com>
2025-04-20 23:12:57 -0400

    Updated buffer, git, and search functionalities in Snacks plugin.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -10,17 +10,21 @@ return {
   },
 
   keys = {
-    -- out of this world interface
-    { '<leader>s/', function() Snacks.picker.lines() end, desc = 'Buffer Lines' },
-
     { '<leader>:', function() Snacks.picker.command_history() end, desc = 'Command History' },
     { '<leader>fp', function() Snacks.picker.projects() end, desc = 'Projects' },
 
     -- git
     { '<leader>gf', function() Snacks.picker.git_log_file() end, desc = 'Git Log File' },
     { '<leader>gl', function() Snacks.picker.git_log() end, desc = 'Git Log' },
+    { '<leader>gg', function() Snacks.lazygit() end, desc = 'Lazygit' },
+    { '<leader>gB', function() Snacks.gitbrowse() end, desc = 'Git Browse', mode = { 'n', 'v' } },
 
+    -- buffer
+    { '<leader>s/', function() Snacks.picker.lines() end, desc = 'Buffer Lines' },
+    { '<leader>su', function() Snacks.picker.undo() end, desc = 'Undo History' },
+    { '<leader>sR', function() Snacks.picker.resume() end, desc = 'Resume' },
     { '<leader>sB', function() Snacks.picker.grep_buffers() end, desc = 'Grep Open Buffers' },
+
     -- search
     { '<leader>s"', function() Snacks.picker.registers() end, desc = 'Registers' },
     { '<leader>sa', function() Snacks.picker.autocmds() end, desc = 'Autocmds' },
@@ -32,21 +36,14 @@ return {
     { '<leader>sm', function() Snacks.picker.marks() end, desc = 'Marks' },
     { '<leader>sp', function() Snacks.picker.lazy() end, desc = 'Search for Plugin Spec' },
     { '<leader>sq', function() Snacks.picker.qflist() end, desc = 'Quickfix List' },
-    { '<leader>sR', function() Snacks.picker.resume() end, desc = 'Resume' },
-    { '<leader>su', function() Snacks.picker.undo() end, desc = 'Undo History' },
+
     { '<leader>uC', function() Snacks.picker.colorschemes() end, desc = 'Colorschemes' },
+
     -- LSP
     { 'gd', function() Snacks.picker.lsp_definitions() end, desc = 'Goto Definition' },
     { 'gD', function() Snacks.picker.lsp_declarations() end, desc = 'Goto Declaration' },
     { 'gI', function() Snacks.picker.lsp_implementations() end, desc = 'Goto Implementation' },
     { 'gy', function() Snacks.picker.lsp_type_definitions() end, desc = 'Goto T[y]pe Definition' },
-
-    { '<leader>bd', function() Snacks.bufdelete() end, desc = 'Delete Buffer' },
-    { '<leader>cR', function() Snacks.rename.rename_file() end, desc = 'Rename File' },
-    { '<leader>gB', function() Snacks.gitbrowse() end, desc = 'Git Browse', mode = { 'n', 'v' } },
-    { '<leader>gg', function() Snacks.lazygit() end, desc = 'Lazygit' },
-    { '<c-/>', function() Snacks.terminal() end, desc = 'Toggle Terminal' },
-    { '<c-_>', function() Snacks.terminal() end, desc = 'which_key_ignore' },
   },
 
   init = function()
@@ -58,7 +55,6 @@ return {
         _G.bt = function() Snacks.debug.backtrace() end
         vim.print = _G.dd -- Override print to use snacks for `:=` command
 
-        -- Create some toggle mappings
         Snacks.toggle.inlay_hints():map '<leader>uh'
         Snacks.toggle.dim():map '<leader>uD'
       end,
