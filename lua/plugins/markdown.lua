f90d558a71ee22789c528c95e029c0bd0c55afe3
Searidang Pa <dangsyncpa@gmail.com>
2025-04-20 20:38:48 -0400

    Update Luasnip configuration for improved snippet management and markdown rendering settings.

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,23 +1,21 @@
 return {
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    event = 'VeryLazy',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    config = function()
-      require('render-markdown').setup {
-        heading = {
-          sign = false,
-          backgrounds = {
-            'RenderMarkdownH2Bg',
-            'RenderMarkdownH1Bg',
-            'RenderMarkdownH2Bg',
-          },
+  'MeanderingProgrammer/render-markdown.nvim',
+  event = 'VeryLazy',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+  config = function()
+    require('render-markdown').setup {
+      heading = {
+        sign = false,
+        backgrounds = {
+          'RenderMarkdownH2Bg',
+          'RenderMarkdownH1Bg',
+          'RenderMarkdownH2Bg',
         },
-        completions = { blink = { enabled = true } },
-        code = {
-          enabled = false,
-        },
-      }
-    end,
-  },
+      },
+      completions = { blink = { enabled = true } },
+      code = {
+        enabled = false,
+      },
+    }
+  end,
 }
