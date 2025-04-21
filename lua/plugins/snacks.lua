88f990752751a2fa2df7388a17ccffdda4bdabd2
Searidang Pa <dangsyncpa@gmail.com>
2025-04-20 23:37:33 -0400

    Updated comments for better organization in snacks.lua plugin mappings.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -13,22 +13,21 @@ return {
     { '<leader>:', function() Snacks.picker.command_history() end, desc = 'Command History' },
     { '<leader>fp', function() Snacks.picker.projects() end, desc = 'Projects' },
 
-    -- git
+    -- === git ===
     { '<leader>gf', function() Snacks.picker.git_log_file() end, desc = 'Git Log File' },
     { '<leader>gl', function() Snacks.picker.git_log() end, desc = 'Git Log' },
     { '<leader>gg', function() Snacks.lazygit() end, desc = 'Lazygit' },
     { '<leader>gB', function() Snacks.gitbrowse() end, desc = 'Git Browse', mode = { 'n', 'v' } },
 
-    -- search in buffers
+    -- === search in buffers ===
     { '<leader>s/', function() Snacks.picker.lines() end, desc = 'Buffer Lines' },
     { '<leader>su', function() Snacks.picker.undo() end, desc = 'Undo History' },
     { '<leader>sR', function() Snacks.picker.resume() end, desc = 'Resume' },
     { '<leader>sB', function() Snacks.picker.grep_buffers() end, desc = 'Grep Open Buffers' },
 
-    -- === search registers, autocmds, command_history, marks, qflist
+    -- === search registers, autocmds, command_history, marks, qflist ===
     { '<leader>s"', function() Snacks.picker.registers() end, desc = 'Registers' },
     { '<leader>sa', function() Snacks.picker.autocmds() end, desc = 'Autocmds' },
-    { '<leader>sc', function() Snacks.picker.command_history() end, desc = 'Command History' },
     { '<leader>sm', function() Snacks.picker.marks() end, desc = 'Marks' },
     { '<leader>sq', function() Snacks.picker.qflist() end, desc = 'Quickfix List' },
 
