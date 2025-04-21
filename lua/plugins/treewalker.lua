ad5d3fa1f289f15da6bec5f6b55ac986dd51ccfd
Searidang Pa <dangsyncpa@gmail.com>
2025-04-20 20:05:57 -0400

    Removed unused keymap variable and updated Treewalker swapping mappings to use `vim.keymap.set`.

--- lua/plugins/treewalker.lua
+++ lua/plugins/treewalker.lua
@@ -1,5 +1,3 @@
-local map = vim.keymap.set
-
 return {
   'aaronik/treewalker.nvim',
   opts = {
@@ -7,16 +5,8 @@ return {
     highlight_group = 'CursorLine',
   },
   config = function()
-    -- Treewalker movement
-    map({ 'n', 'v' }, '<M-k>', '<cmd>Treewalker Up<cr>', { silent = true })
-    map({ 'n', 'v' }, '<M-j>', '<cmd>Treewalker Down<cr>', { silent = true })
-    map({ 'n', 'v' }, '<M-h>', '<cmd>Treewalker Left<cr>', { silent = true })
-    map({ 'n', 'v' }, '<M-l>', '<cmd>Treewalker Right<cr>', { silent = true })
-
     -- Treewalker swapping
-    map('n', '<M-S-k>', '<cmd>Treewalker SwapUp<cr>', { silent = true })
-    map('n', '<M-S-j>', '<cmd>Treewalker SwapDown<cr>', { silent = true })
-    map('n', '<M-S-h>', '<cmd>Treewalker SwapLeft<cr>', { silent = true })
-    map('n', '<M-S-l>', '<cmd>Treewalker SwapRight<cr>', { silent = true })
+    vim.keymap.set('n', '<M-S-h>', '<cmd>Treewalker SwapLeft<cr>', { silent = true })
+    vim.keymap.set('n', '<M-S-l>', '<cmd>Treewalker SwapRight<cr>', { silent = true })
   end,
 }
