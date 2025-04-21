fc8f585d683354bc54fe0478b3984980936d9327
Searidang Pa <dangsyncpa@gmail.com>
2025-04-20 22:36:50 -0400

    Removed unused git-related keybindings from snacks.lua.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -16,11 +16,6 @@ return {
     -- git
     { '<leader>gf', function() Snacks.picker.git_log_file() end, desc = 'Git Log File' },
     { '<leader>gl', function() Snacks.picker.git_log() end, desc = 'Git Log' },
-    { '<leader>gL', function() Snacks.picker.git_log_line() end, desc = 'Git Log Line' },
-    { '<leader>gs', function() Snacks.picker.git_status() end, desc = 'Git Status' },
-    { '<leader>gS', function() Snacks.picker.git_stash() end, desc = 'Git Stash' },
-    { '<leader>gd', function() Snacks.picker.git_diff() end, desc = 'Git Diff (Hunks)' },
-    -- Grep
 
     { '<leader>s/', function() Snacks.picker.lines() end, desc = 'Buffer Lines' },
     { '<leader>sB', function() Snacks.picker.grep_buffers() end, desc = 'Grep Open Buffers' },
