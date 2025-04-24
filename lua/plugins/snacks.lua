c7c0c148101f05b4acca91e3d92fcaa7bae275d6
Searidang Pa <dangsyncpa@gmail.com>
2025-04-24 13:19:45 -0400

    Add new keybindings for advanced search functionalities in Snacks plugin and remove redundant mappings in Telescope setup.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -55,20 +55,33 @@ return {
     },
     { '<leader>sf', function() Snacks.picker.files() end, desc = 'Find Files' },
     { '<leader>sg', function() Snacks.picker.grep() end, desc = 'Grep' },
+    {
+      '<leader>sG',
+      function()
+        Snacks.picker.grep {
+          cwd = vim.fs.joinpath(tostring(vim.fn.stdpath 'data'), 'lazy'),
+        }
+      end,
+      desc = 'Grep',
+    },
     { '<leader>so', function() Snacks.picker.grep_buffers() end, desc = 'Grep Open Buffers' },
+    { '<leader>sO', function() Snacks.picker.buffers() end, desc = 'Grep Open Buffers' },
     { '<leader>sd', function() Snacks.picker.diagnostics() end, desc = 'Diagnostics' },
     { '<leader>se', function() Snacks.picker.git_status() end, desc = 'Git Stash' },
+    { '<leader>sw', function() Snacks.picker.grep_word() end, desc = 'Visual selection or word', mode = { 'n', 'x' } },
 
     -- === git ===
     { '<leader>gf', function() Snacks.picker.git_log_file() end, desc = 'Git Log File' },
     { '<leader>gl', function() Snacks.picker.git_log() end, desc = 'Git Log' },
     { '<leader>gg', function() Snacks.lazygit() end, desc = 'Lazygit' },
-    { '<leader>gB', function() Snacks.gitbrowse() end, desc = 'Git Browse', mode = { 'n', 'v' } },
+    { '<leader>gd', function() Snacks.picker.git_diff() end, desc = 'Git Diff (Hunks)' },
 
     -- === search in buffers ===
     { '<leader>s/', function() Snacks.picker.lines() end, desc = 'Buffer Lines' },
     { '<leader>su', function() Snacks.picker.undo() end, desc = 'Undo History' },
-    { '<leader>sR', function() Snacks.picker.resume() end, desc = 'Resume' },
+    { '<leader>sr', function() Snacks.picker.resume() end, desc = 'Resume' },
+    { '<leader>sh', function() Snacks.picker.help() end, desc = 'Help Pages' },
+    { '<leader>sk', function() Snacks.picker.keymaps() end, desc = 'Keymaps' },
 
     -- === search registers, autocmds, command_history, marks, qflist ===
     { '<leader>s"', function() Snacks.picker.registers() end, desc = 'Registers' },
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -16,61 +16,8 @@ return {
 
   config = function()
     local builtin = require 'telescope.builtin'
-    local map = vim.keymap.set
-    local map_modes = { 'n', 'v' }
-    local function grep_plugins()
-      builtin.live_grep {
-        cwd = vim.fs.joinpath(tostring(vim.fn.stdpath 'data'), 'lazy'),
-      }
-    end
-
-    -- help
-    map(map_modes, '<leader>sG', grep_plugins, { desc = '[S]earch Plugin by [G]rep' })
-    map(map_modes, '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-    map(map_modes, '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-
-    -- === jumping around ===
-    map(map_modes, '<leader>sO', builtin.buffers, { desc = '[S]earch [O]pen Buffers' })
-
-    map(map_modes, '<leader>sw', builtin.grep_string, { desc = '[S]earch [C]urrent word' })
-    map(map_modes, '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-
-    -- === git ===
-    map(map_modes, '<leader>sc', builtin.git_commits, { desc = '[S]earch [C]ommits' })
-    map(map_modes, '<leader>sl', builtin.git_bcommits_range, { desc = '[S]earch last commits on this [L]ine' })
-
-    -- === LSP ===
-
     local opts = {
-      pickers = {
-        buffers = { theme = 'ivy' },
-        current_buffer_fuzzy_find = { theme = 'ivy' },
-        help_tags = { theme = 'ivy' },
-        find_files = { theme = 'ivy', hidden = true },
-        live_grep = { theme = 'ivy', hidden = true },
-        oldfiles = { theme = 'ivy' },
-        jumplist = { theme = 'ivy' },
-        keymaps = { theme = 'ivy' },
-        diagnostics = { theme = 'ivy' },
-        lsp_references = { theme = 'ivy', file_ignore_patterns = { '%.pb.go' } },
-        lsp_dynamic_workspace_symbols = { theme = 'ivy' },
-        lsp_document_symbols = { theme = 'ivy' },
-        lsp_incoming_calls = { theme = 'ivy' },
-        grep_string = { theme = 'ivy' },
-        git_bcommits = { theme = 'ivy' },
-        git_commits = { theme = 'ivy' },
-        git_bcommits_range = { theme = 'ivy' },
-        git_status = { theme = 'ivy' },
-
-        git_branches = {
-          theme = 'ivy',
-        },
-      },
       defaults = {
-        file_ignore_patterns = {
-          '.git\\',
-          'git/',
-        },
         layout_strategy = 'vertical',
         layout_config = {
           width = 0.7,
