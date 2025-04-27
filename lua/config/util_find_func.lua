7ec07e9762285d5760f039575fd936e161836636
Searidang Pa <dangsyncpa@gmail.com>
2025-04-26 22:45:20 -0400

    Update Lua version requirement to 5.1 in .tool-versions and streamline util_find_func.lua by removing unused query strings and optimizing function implementations.

--- lua/config/util_find_func.lua
+++ lua/config/util_find_func.lua
@@ -1,82 +1,7 @@
 local M = {}
 
 local get_node_text = vim.treesitter.get_node_text
-local ts_utils = require('nvim-treesitter.ts_utils')
-
-local test_function_query_string = [[
-(function_declaration
-  name: (identifier) @name
-  parameters: (parameter_list
-    (parameter_declaration
-      name: (identifier)
-      type: (pointer_type
-        (qualified_type
-          package: (package_identifier) @_package_name
-          name: (type_identifier) ))))
-  (#eq? @_package_name "testing")
-  (#eq? @name "%s")
-)
-]]
-
-Find_test_line_by_name = function(go_bufnr, testName)
-  local formatted = string.format(test_function_query_string, testName)
-  local query = vim.treesitter.query.parse('go', formatted)
-  local parser = vim.treesitter.get_parser(go_bufnr, 'go', {})
-  assert(parser, 'parser is nil')
-  local tree = parser:parse()[1]
-  local root = tree:root()
-
-  for _, node in query:iter_captures(root, go_bufnr, 0, -1) do
-    local nodeContent = get_node_text(node, go_bufnr)
-
-    if nodeContent == testName then
-      local start_line, _, _, _ = node:range()
-      return start_line + 1
-    end
-  end
-end
-
--- maybe this is not needed
-local function_query = [[
-(function_declaration
-  name: (identifier) @name
-  parameters: (parameter_list
-    (parameter_declaration
-      name: (identifier)
-      type: (pointer_type
-        (qualified_type
-          package: (package_identifier) @_package_name
-          name: (type_identifier) ))))
-)
-]]
-
-function Find_all_tests (go_bufnr)
-  local query = vim.treesitter.query.parse('go', function_query)
-  local parser = vim.treesitter.get_parser(go_bufnr, 'go', {})
-  assert(parser, 'parser is nil')
-  local tree = parser:parse()[1]
-  local root = tree:root()
-  assert(root, 'root is nil')
-
-  local res = {}
-  for _, node in query:iter_captures(root, go_bufnr, 0, -1) do
-    if node == nil then
-      return res
-    end
-    local nodeContent = get_node_text(node, go_bufnr)
-
-    -- all tests start with Test_
-    if not string.match(nodeContent, 'Test_') then
-      goto continue
-    end
-
-    res[nodeContent] = node:start() + 1
-    ::continue::
-  end
-  return res
-end
-
-
+local ts_utils = require 'nvim-treesitter.ts_utils'
 
 ---@param bufnr number
 ---@param line number
@@ -84,6 +9,7 @@ end
 M.nearest_function_at_line = function(bufnr, line)
   local lang = vim.treesitter.language.get_lang(vim.bo[bufnr].filetype) -- Get language from filetype
   local parser = vim.treesitter.get_parser(bufnr, lang)
+  assert(parser, 'parser is nil')
   local tree = parser:parse()[1]
   local root = tree:root()
   assert(tree, 'tree is nil')
@@ -120,17 +46,15 @@ function Nearest_func_name()
   local func_node = M.nearest_function_at_line(bufnr, line)
   assert(func_node, 'No function found')
   for child in func_node:iter_children() do
-      if child:type() == 'identifier' or child:type() == 'field_identifier' or child:type() == 'name' then
-          return vim.treesitter.get_node_text(child, bufnr)
-      end
-      if child:type() == 'dot_index_expression' then
-      end
+    if child:type() == 'identifier' or child:type() == 'field_identifier' or child:type() == 'name' then
+      return vim.treesitter.get_node_text(child, bufnr)
+    end
   end
 end
 
 vim.api.nvim_create_user_command('NearestFuncName', function()
   local func_name = Nearest_func_name()
-  print("Nearest func name: " .. func_name)
+  print('Nearest func name: ' .. func_name)
 end, {})
 
 ---@return TSNode
@@ -163,22 +87,6 @@ function Get_enclosing_fn_info()
   return nil
 end
 
-function Get_enclosing_test()
-  local test_line, testName = Get_enclosing_fn_info()
-  if not testName then
-    print 'Not in a function'
-    return nil
-  end
-  if not string.match(testName, 'Test_') then
-    print(string.format('Not in a test function: %s', testName))
-    return nil
-  end
-  return testName, test_line
-end
-
-
-local map = vim.keymap.set
-
 M.visual_function = function()
   local func_node = Nearest_func_node()
   local start_row, start_col, end_row, end_col = func_node:range()
@@ -193,8 +101,8 @@ M.delete_function = function()
   vim.cmd 'normal! d'
 end
 
+local map = vim.keymap.set
 map('n', '<leader>vf', M.visual_function, { desc = 'Visual nearest function' })
 map('n', '<leader>df', M.delete_function, { desc = 'Delete nearest function' })
 
 return M
-
