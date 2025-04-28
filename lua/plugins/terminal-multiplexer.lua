a5c97f69cf6c26dccec70375ab1ed0ba4b91fd00
Searidang Pa <dangsyncpa@gmail.com>
2025-04-27 22:09:09 -0400

    Debugging

--- lua/plugins/terminal-multiplexer.lua
+++ lua/plugins/terminal-multiplexer.lua
@@ -1,6 +1,7 @@
 return {
   'SearidangPa/terminal-multiplexer.nvim',
   lazy = true,
+  event = 'VeryLazy',
   config = function()
     require 'custom.terminals_daemon'
     require 'custom.git_flow'
