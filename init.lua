a09edcc72cded9919a172ebcd653f780d0e4d444
Searidang Pa <dangsyncpa@gmail.com>
2025-04-28 18:17:08 -0400

    Pushing to remote

--- init.lua
+++ init.lua
@@ -2,7 +2,46 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 vim.g.copilot_no_tab_map = true -- copilot plugin config
 vim.g.undotree_WindowLayout = 2 -- undo tree plugin config
-require 'vim_opt'
+
+--- === case insensitive search, unless capital letter is used ===
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- === Time Periods ===
+vim.opt.timeoutlen = 300
+vim.opt.updatetime = 50
+
+-- === Files ===
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
+-- === UI ===
+vim.opt.list = true
+vim.opt.listchars = { tab = '   ', trail = '·', nbsp = '␣' }
+vim.opt.scrolloff = 10
+vim.opt.numberwidth = 1
+vim.opt.tabstop = 6
+
+--- === Live Preview ===
+vim.opt.inccommand = 'nosplit' -- Preview substitutions live, as you type!
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- === Duh ===
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.mouse = 'a'
+vim.g.have_nerd_font = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = 'yes'
+vim.opt.cursorline = true
+vim.opt.breakindent = true
+
+-- === luarocks ===
+
+vim.schedule(function() vim.opt.clipboard = 'unnamedplus' end)
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
