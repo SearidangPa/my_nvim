c0bf6099155ab32ca190ca2fcfe057864c1bd46a
Searidang Pa <dangsyncpa@gmail.com>
2025-04-29 11:02:01 -0400

    Removed global debug functions and lazy loading setup from snacks.lua plugin. Simplified the return statement by removing unnecessary init function and associated autocmd.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -154,19 +154,4 @@ return {
     { '<leader>ds', function() Snacks.picker.lsp_symbols() end, desc = 'LSP Symbols' },
     { '<leader>ws', function() Snacks.picker.lsp_workspace_symbols() end, desc = 'LSP Workspace Symbols' },
   },
-
-  init = function()
-    vim.api.nvim_create_autocmd('User', {
-      pattern = 'VeryLazy',
-      callback = function()
-        -- Setup some globals for debugging (lazy-loaded)
-        _G.dd = function(...) Snacks.debug.inspect(...) end
-        _G.bt = function() Snacks.debug.backtrace() end
-        vim.print = _G.dd -- Override print to use snacks for `:=` command
-
-        require 'snacks'
-        Snacks.toggle.dim():map '<leader>td'
-      end,
-    })
-  end,
 }
