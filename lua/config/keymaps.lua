fe2be38eec7df057f5112742b3bea66501905db1
Searidang Pa <dangsyncpa@gmail.com>
2025-04-29 15:01:21 -0400

    "Refactor keybindings: Remove unused local leader mappings and update <C-s> to write all files."

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -76,9 +76,7 @@ map({ 'n', 'i' }, '<C-space>', function() vim.lsp.buf.signature_help() end, map_
 map('n', '<leader>rc', RenameAndCapitalize, map_opt '[R]ename and [C]apitalize first character')
 map('n', '<leader>rl', RenameAndLowercase, map_opt '[R]ename and [L]owercase first character')
 
--- ================== local leader===================
-map('n', '<D-s>', function() vim.cmd [[:wa]] end, map_opt '[W]rite all')
-map('n', '<M-s>', function() vim.cmd [[:wa]] end, map_opt '[W]rite all')
+map('n', '<C-s>', function() vim.cmd [[:wa]] end, map_opt '[W]rite all')
 
 map({ 'v', 'x' }, '<localleader>d', [["_x]], map_opt '[D]elete into black hole')
 map('n', '<localleader>xx', '<cmd>source %<CR>', map_opt '[E]xecute current lua file')
