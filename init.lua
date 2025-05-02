d5960ae75882ccb9e16a5871e0868f65daa9811a
Searidang Pa <dangsyncpa@gmail.com>
2025-05-01 21:38:39 -0400

    Swap mapleader and maplocalleader

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ','
+vim.g.mapleader = ','
+vim.g.maplocalleader = ' '
 vim.g.copilot_no_tab_map = true -- copilot plugin config
 vim.g.undotree_WindowLayout = 2 -- undo tree plugin config
 
