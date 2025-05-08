96ab960e1fc19800287c413b4d6e7c375876b141
Searidang Pa <dangsyncpa@gmail.com>
2025-05-08 15:43:21 -0400

    Remove grep command, add lazygit for git branches

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -67,21 +67,13 @@ return {
       end,
       desc = 'Find Neovim Config Files',
     },
-    {
-      '<leader>sG',
-      function()
-        Snacks.picker.grep {
-          cwd = vim.fs.joinpath(tostring(vim.fn.stdpath 'data'), 'lazy'),
-        }
-      end,
-      desc = 'Grep',
-    },
 
     -- === git ===
     { '<leader>gf', function() Snacks.picker.git_log_file() end, desc = 'Git Log File' },
     { '<leader>gl', function() Snacks.picker.git_log() end, desc = 'Git Log' },
     { '<leader>gd', function() Snacks.picker.git_diff() end, desc = 'Git Diff (Hunks)' },
     { '<leader>gb', function() Snacks.picker.git_branches() end, desc = 'Git Branches' },
+    { '<leader>gg', function() Snacks.lazygit() end, desc = 'Git Branches' },
 
     -- === LSP ===
     { 'gd', function() Snacks.picker.lsp_definitions() end, desc = 'Goto Definition' },
