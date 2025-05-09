61cb224eb62fa1b27465c6104356822092366117
Searidang Pa <dangsyncpa@gmail.com>
2025-05-09 13:31:25 -0400

    Update hopcopilot event to BufReadPost, adjust theme plugin config and order.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,29 +1,27 @@
 return {
   {
     'SearidangPa/theme-loader.nvim',
-    dependencies = {
-      {
-        'catppuccin/nvim',
-        name = 'catppuccin',
-        config = true,
-        lazy = true,
-      },
+    priority = 1000,
+    config = function() require('theme-loader').setup() end,
+  },
 
-      {
-        'rose-pine/neovim',
-        name = 'rose-pine',
-        lazy = true,
-        config = true,
-        opts = {
-          variant = 'moon',
-          styles = {
-            italic = false,
-          },
-        },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    config = true,
+    lazy = true,
+  },
+
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    lazy = true,
+    config = true,
+    opts = {
+      variant = 'moon',
+      styles = {
+        italic = false,
       },
     },
-    config = function() require('theme-loader').setup() end,
-    lazy = true,
-    event = 'VeryLazy',
   },
 }
