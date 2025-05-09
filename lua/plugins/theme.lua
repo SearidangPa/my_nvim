7e7a770115e72baab8026ef7a9e7c8b8b3ad9672
Searidang Pa <dangsyncpa@gmail.com>
2025-05-09 15:53:35 -0400

    Removed theme-loader dependency, reorganized plugin order, added moon variant and italic style config for rose-pine.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,27 +1,25 @@
 return {
   {
-    'SearidangPa/theme-loader.nvim',
-    dependencies = {
-      {
-        'catppuccin/nvim',
-        name = 'catppuccin',
-        config = true,
-        lazy = true,
-      },
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    config = true,
+    lazy = true,
+  },
 
-      {
-        'rose-pine/neovim',
-        name = 'rose-pine',
-        lazy = true,
-        config = true,
-        opts = {
-          variant = 'moon',
-          styles = {
-            italic = false,
-          },
-        },
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    lazy = true,
+    config = true,
+    opts = {
+      variant = 'moon',
+      styles = {
+        italic = false,
       },
     },
+  },
+  {
+    'SearidangPa/theme-loader.nvim',
     lazy = false,
     priority = 1000,
     config = function() require('theme-loader').setup() end,
