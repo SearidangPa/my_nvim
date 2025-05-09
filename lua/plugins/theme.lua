a0d90f491f960c4ef3853a22f0987006941264a0
Searidang Pa <dangsyncpa@gmail.com>
2025-05-09 13:25:13 -0400

    Refactor harpoon setup to remove unused conditional logic. Update theme loader to use dependencies for catppuccin and rose-pine.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,26 +1,29 @@
 return {
   {
     'SearidangPa/theme-loader.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function() require('theme-loader').setup() end,
-  },
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    config = true,
-    lazy = true,
-  },
-  {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    lazy = true,
-    config = true,
-    opts = {
-      variant = 'moon',
-      styles = {
-        italic = false,
+    dependencies = {
+      {
+        'catppuccin/nvim',
+        name = 'catppuccin',
+        config = true,
+        lazy = true,
+      },
+
+      {
+        'rose-pine/neovim',
+        name = 'rose-pine',
+        lazy = true,
+        config = true,
+        opts = {
+          variant = 'moon',
+          styles = {
+            italic = false,
+          },
+        },
       },
     },
+    config = function() require('theme-loader').setup() end,
+    lazy = true,
+    event = 'VeryLazy',
   },
 }
