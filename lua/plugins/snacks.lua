c63b107fb257c57d62a9e45ad3b526f6609ef23f
Searidang Pa <dangsyncpa@gmail.com>
2025-05-10 11:16:44 -0400

    Rename git diff command keybinding from `gd` to `e` for clarity and to avoid conflict.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -69,7 +69,7 @@ return {
     -- === git ===
     { '<leader>gf', function() Snacks.picker.git_log_file() end, desc = 'Git Log File' },
     { '<leader>gl', function() Snacks.picker.git_log() end, desc = 'Git Log' },
-    { '<leader>gd', function() Snacks.picker.git_diff() end, desc = 'Git Diff (Hunks)' },
+    { '<leader>e', function() Snacks.picker.git_diff() end, desc = 'Git Diff Hunks (Edited)' },
     { '<leader>gb', function() Snacks.picker.git_branches() end, desc = 'Git Branches' },
     { '<leader>gg', function() Snacks.lazygit() end, desc = 'Git Branches' },
 
