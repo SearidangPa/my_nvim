e8c2d5beb969eefa1f808c5ee70b122587caff50
Searidang Pa <dangsyncpa@gmail.com>
2025-05-10 11:16:08 -0400

    Rename `gD` to `gI` for Goto Implementation, and update `gD` to `gI` for consistency.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -75,9 +75,8 @@ return {
 
     -- === LSP ===
     { 'gd', function() Snacks.picker.lsp_definitions() end, desc = 'Goto Definition' },
-    { 'gD', function() Snacks.picker.lsp_declarations() end, desc = 'Goto Declaration' },
+    { 'gD', function() Snacks.picker.lsp_type_definitions() end, desc = 'Goto Declaration' },
     { 'gI', function() Snacks.picker.lsp_implementations() end, desc = 'Goto Implementation' },
-    { '<leader>D', function() Snacks.picker.lsp_type_definitions() end, desc = 'Goto T[y]pe Definition' },
 
     { '<leader>uc', function() Snacks.picker.colorschemes() end, desc = 'Colorschemes' },
 
