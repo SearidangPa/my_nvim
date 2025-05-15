e967e16d92d16daf239a2ef2085c51f98991d67f
Searidang Pa <dangsyncpa@gmail.com>
2025-05-15 09:48:25 -0400

    Add new keymaps for running tests and jumping to last test in code.

--- lua/plugins/go-test-t.lua
+++ lua/plugins/go-test-t.lua
@@ -9,10 +9,27 @@ return {
       go_test_prefix = 'MODE=staging UKS=others go test'
     end
 
-    require('go-test-t').new {
+    local go_test_t = require('go-test-t').new {
       go_test_prefix = go_test_prefix,
       user_command_prefix = 'Go',
     }
+
     vim.keymap.set('n', '<localleader>g', ':GoTestTerm<CR>', { desc = 'Go testies' })
+
+    vim.keymap.set('n', '<localleader>t', function()
+      local util_lsp = require 'go-test-t.util_lsp'
+      local test_name = go_test_t.term_tester.terminal_multiplexer.last_terminal_name
+      util_lsp.action_from_test_name(test_name, function(lsp_param)
+        local filepath = lsp_param.filepath
+        local test_line = lsp_param.test_line
+        vim.cmd('edit ' .. filepath)
+
+        if test_line then
+          local pos = { test_line, 0 }
+          vim.api.nvim_win_set_cursor(0, pos)
+          vim.cmd 'normal! zz'
+        end
+      end)
+    end, { desc = 'jump to last test in code' })
   end,
 }
