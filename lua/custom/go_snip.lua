0bb8c9db7f64c1daa9ec9ad752c0e05fbd275d80
Searidang Pa <dangsyncpa@gmail.com>
2025-05-19 11:31:11 -0400

    Remove unused snippet node reference

--- lua/custom/go_snip.lua
+++ lua/custom/go_snip.lua
@@ -5,7 +5,6 @@ local c = ls.choice_node
 local d = ls.dynamic_node
 local i = ls.insert_node
 local s = ls.snippet
-local sn = ls.snippet_node
 local f = ls.function_node
 local t = ls.text_node
 local extras = require 'luasnip.extras'
