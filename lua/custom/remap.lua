910e8c223e364ce8c0d275b70cce66dab7515cce
Searidang Pa <dangsyncpa@gmail.com>
2025-05-21 13:29:40 -0400

    Add terminal exit mapping, fix paste mapping for visual mode, and adjust line movement mappings.

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -2,6 +2,7 @@ local map = vim.keymap.set
 local function map_opt(desc) return { noremap = true, silent = true, desc = desc } end
 
 map('i', '<C-D>', '<Del>', map_opt 'Delete character under the cursor')
+map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }) -- exit terminal mode
 
 -- === tag zz at the end ===
 map('n', '<C-u>', '<C-u>zz')
@@ -14,13 +15,10 @@ map('n', 'n', 'nzzzv')
 map('n', 'j', [[(v:count > 1 ? 'm`' . v:count : 'g') . 'j']], { expr = true })
 map('n', 'k', [[(v:count > 1 ? 'm`' . v:count : 'g') . 'k']], { expr = true })
 
--- === move a line in visual mode ===
+-- === visual mode ===
 map('v', 'J', ":m '>+1<CR>gv=gv") -- move line down
 map('v', 'K', ":m '<-2<CR>gv=gv") -- move line up
-
-map('x', '<leader>p', [["_dP]], map_opt '[P]aste without overwriting the clipboard')
-
-map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }) -- exit terminal mode
+map('x', 'p', [["_dP]], map_opt '[P]aste without overwriting the clipboard')
 
 map('n', 'gk', 'O<Esc>j', map_opt 'Insert empty line above')
 map('n', 'gj', 'o<Esc>k', map_opt 'Insert empty line below')
