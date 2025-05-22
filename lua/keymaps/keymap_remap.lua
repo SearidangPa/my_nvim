a90cc76b43883b0813a461f0e27be6ec0ce8e6c5
Searidang Pa <dangsyncpa@gmail.com>
2025-05-22 15:30:52 -0400

    Refactor keymap functions to use set_map_key instead of map for consistency and clarity.

--- lua/keymaps/keymap_remap.lua
+++ lua/keymaps/keymap_remap.lua
@@ -1,6 +1,6 @@
-local map = vim.keymap.set
+local set_map_key = vim.keymap.set
 local function map_opt(desc) return { noremap = true, silent = true, desc = desc } end
-map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+set_map_key('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 local function save_all()
   if vim.bo.filetype == 'snacks_input' then
@@ -9,43 +9,43 @@ local function save_all()
   end
   vim.cmd 'silent! wa'
 end
-map('n', '<Enter>', save_all, { desc = 'Save all buffers' })
+set_map_key('n', '<Enter>', save_all, { desc = 'Save all buffers' })
 
-map('n', '<Tab>', 'zA', map_opt 'Toggle fold')
-map('n', ']]', 'zj', map_opt 'Next fold')
-map('n', '[[', 'zk', map_opt 'Previous fold')
+set_map_key('n', '<Tab>', 'zA', map_opt 'Toggle fold')
+set_map_key('n', ']]', 'zj', map_opt 'Next fold')
+set_map_key('n', '[[', 'zk', map_opt 'Previous fold')
 
-map('n', '<leader>ce', ':ClearExtmarks<CR>', map_opt '[C]lear [E]xtmarks')
-map('n', '<BS>', ':messages<CR>', map_opt 'Show [M]essages')
-map('x', '/', '<Esc>/\\%V', { desc = 'Search in visual selection' })
+set_map_key('n', '<leader>ce', ':ClearExtmarks<CR>', map_opt '[C]lear [E]xtmarks')
+set_map_key('n', '<BS>', ':messages<CR>', map_opt 'Show [M]essages')
+set_map_key('x', '/', '<Esc>/\\%V', { desc = 'Search in visual selection' })
 
 -- === Insert mode
-map('i', '<C-D>', '<Del>', map_opt 'Delete character under the cursor')
-map({ 'n', 'i' }, '<C-space>', function() vim.lsp.buf.signature_help() end, map_opt 'Signature help')
+set_map_key('i', '<C-D>', '<Del>', map_opt 'Delete character under the cursor')
+set_map_key({ 'n', 'i' }, '<C-space>', function() vim.lsp.buf.signature_help() end, map_opt 'Signature help')
 
 -- === navigation: tag zz at the end
-map('n', '<C-u>', '<C-u>zz')
-map('n', '<C-d>', '<C-d>zz')
-map('n', '<PageUp>', '<C-u>zz')
-map('n', '<PageDown>', '<C-d>zz')
-map('n', 'n', 'nzzzv')
+set_map_key('n', '<C-u>', '<C-u>zz')
+set_map_key('n', '<C-d>', '<C-d>zz')
+set_map_key('n', '<PageUp>', '<C-u>zz')
+set_map_key('n', '<PageDown>', '<C-d>zz')
+set_map_key('n', 'n', 'nzzzv')
 
 -- === navigation long wrapped lines
-map('n', 'j', [[(v:count > 1 ? 'm`' . v:count : 'g') . 'j']], { expr = true })
-map('n', 'k', [[(v:count > 1 ? 'm`' . v:count : 'g') . 'k']], { expr = true })
+set_map_key('n', 'j', [[(v:count > 1 ? 'm`' . v:count : 'g') . 'j']], { expr = true })
+set_map_key('n', 'k', [[(v:count > 1 ? 'm`' . v:count : 'g') . 'k']], { expr = true })
 
 -- === visual mode
-map('v', 'J', ":m '>+1<CR>gv=gv") -- move line down
-map('v', 'K', ":m '<-2<CR>gv=gv") -- move line up
+set_map_key('v', 'J', ":m '>+1<CR>gv=gv") -- move line down
+set_map_key('v', 'K', ":m '<-2<CR>gv=gv") -- move line up
 
 -- === visual mode: delete, paste into black hole
-map({ 'x', 'v' }, 'p', [["_dP]], map_opt '[P]aste without overwriting the clipboard')
-map({ 'v', 'x' }, '<leader>d', [["_x]], map_opt '[D]elete into black hole')
+set_map_key({ 'x', 'v' }, 'p', [["_dP]], map_opt '[P]aste without overwriting the clipboard')
+set_map_key({ 'v', 'x' }, '<leader>d', [["_x]], map_opt '[D]elete into black hole')
 
 -- === window management
-map('n', '<C-h>', '<C-w><C-h>', map_opt 'Move focus to the left window')
-map('n', '<C-l>', '<C-w><C-l>', map_opt 'Move focus to the right window')
-map('n', '<D-j>', '<C-w><C-j>', map_opt 'Move focus to the below window')
-map('n', '<D-k>', '<C-w><C-k>', map_opt 'Move focus to the above window')
-map('n', '<M-j>', '<C-w><C-j>', map_opt 'Move focus to the below window')
-map('n', '<M-k>', '<C-w><C-k>', map_opt 'Move focus to the above window')
+set_map_key('n', '<C-h>', '<C-w><C-h>', map_opt 'Move focus to the left window')
+set_map_key('n', '<C-l>', '<C-w><C-l>', map_opt 'Move focus to the right window')
+set_map_key('n', '<D-j>', '<C-w><C-j>', map_opt 'Move focus to the below window')
+set_map_key('n', '<D-k>', '<C-w><C-k>', map_opt 'Move focus to the above window')
+set_map_key('n', '<M-j>', '<C-w><C-j>', map_opt 'Move focus to the below window')
+set_map_key('n', '<M-k>', '<C-w><C-k>', map_opt 'Move focus to the above window')
