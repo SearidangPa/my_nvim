6d7684b3bb10a43b21ecd03aa64493f10483cd9a
Searidang Pa <dangsyncpa@gmail.com>
2025-05-22 13:11:29 -0400

    Move NewScratch command and yank highlight autocmd to custom/auto_cmd.lua

--- init.lua
+++ init.lua
@@ -82,22 +82,6 @@ vim.api.nvim_create_autocmd('UIEnter', {
     end
     load_modules 'keymaps'
     load_modules 'custom'
-
     vim.treesitter.language.register('bash', 'zsh')
-
-    --- === New Scratch Buffer ===
-    local new_scratch_buf = function()
-      vim.cmd 'enew'
-      vim.cmd 'setlocal buftype=nofile bufhidden=hide noswapfile'
-      vim.cmd 'setlocal filetype=markdown'
-    end
-    vim.api.nvim_create_user_command('NewScratch', new_scratch_buf, { desc = 'Start a scratch buffer' })
-
-    local yank_group = vim.api.nvim_create_augroup('HighlightYank', { clear = true })
-    vim.api.nvim_create_autocmd('TextYankPost', {
-      desc = 'Highlight when yanking (copying) text',
-      group = yank_group,
-      callback = function() vim.highlight.on_yank() end,
-    })
   end,
 })
