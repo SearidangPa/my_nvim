b971da6bc070871e3d46b1e6bd8169c90f60bed2
Searidang Pa <dangsyncpa@gmail.com>
2025-05-22 14:38:36 -0400

    Refactor Tab and Shift+Tab to toggle folds, add leader+Tab for fold navigation, remove empty line insert keymaps

--- lua/keymaps/remap.lua
+++ lua/keymaps/remap.lua
@@ -1,7 +1,10 @@
 local map = vim.keymap.set
 local function map_opt(desc) return { noremap = true, silent = true, desc = desc } end
 
-map('n', '<Tab>', function() vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('za', true, false, true), 'n', true) end, map_opt 'Toggle fold')
+map('n', '<Tab>', 'zA', map_opt 'Toggle fold')
+map('n', '<S-Tab>', 'zj', map_opt 'Toggle fold')
+map('n', '<localleader><Tab>', 'zj', map_opt 'Toggle fold')
+
 map('n', '<leader><BS>', ':ClearExtmarks<CR>', map_opt '[C]lear [E]xtmarks')
 map('n', '<BS>', ':messages<CR>', map_opt 'Show [M]essages')
 map('n', '<Enter>', '<cmd>silent! wa<cr>', { desc = 'Save all buffers' })
@@ -29,10 +32,6 @@ map('v', 'K', ":m '<-2<CR>gv=gv") -- move line up
 -- === visual mode: delete, paste into black hole
 map({ 'x', 'v' }, 'p', [["_dP]], map_opt '[P]aste without overwriting the clipboard')
 map({ 'v', 'x' }, '<leader>d', [["_x]], map_opt '[D]elete into black hole')
-
--- === add empty lines
-map('n', 'gk', 'O<Esc>j', map_opt 'Insert empty line above')
-map('n', 'gj', 'o<Esc>k', map_opt 'Insert empty line below')
 
 -- === window management
 map('n', '<C-h>', '<C-w><C-h>', map_opt 'Move focus to the left window')
