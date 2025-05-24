df434165066d6557a241c098d135b4202f1f27ad
Searidang Pa <dangsyncpa@gmail.com>
2025-05-24 13:38:08 -0400

    Rename function `load_decl_func_reference` to `load_def_of_func_ref` for clarity and consistency.

--- lua/custom/quickfix_func_ref_decl.lua
+++ lua/custom/quickfix_func_ref_decl.lua
@@ -92,7 +92,7 @@ local function find_enclosing_function(uri, ref_line)
   }
 end
 
-local function load_decl_func_reference(bufnr, line, col)
+local function load_def_of_func_ref(bufnr, line, col)
   assert(line, 'line is nil')
   assert(col, 'col is nil')
   local params = {
@@ -141,7 +141,7 @@ function M.load_func_refs()
       end
     end
     local start_row, start_col = func_identifier:range()
-    load_decl_func_reference(vim.api.nvim_get_current_buf(), start_row + 1, start_col + 1)
+    load_def_of_func_ref(vim.api.nvim_get_current_buf(), start_row + 1, start_col + 1)
     return
   end
 
@@ -152,7 +152,7 @@ function M.load_func_refs()
     local filename = item.filename
     local bufnr = vim.fn.bufadd(filename)
     vim.fn.bufload(bufnr)
-    load_decl_func_reference(bufnr, item.lnum, item.col)
+    load_def_of_func_ref(bufnr, item.lnum, item.col)
   end
 end
 
